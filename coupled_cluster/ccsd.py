2aeefcdd12e390eb120a8ebc10467ed2bceb4ae7
Mint-kontor <oyvindschoyen@gmail.com>
2018-09-21 16:47:16 +0200

    Avoid division by D when not iterative

--- coupled_cluster/ccsd.py
+++ coupled_cluster/ccsd.py
@@ -194,6 +194,12 @@ class CoupledClusterSinglesDoubles(CoupledCluster):
         self._compute_ccsd_amplitude_s(iterative=iterative)
         self._compute_ccsd_amplitude_d()
 
+        if not iterative:
+            np.copyto(self.t_1, self.rhs_1)
+            np.copyto(self.t_2, self.rhs_2)
+
+            return
+
         amplitude_scaling_one_body(self.rhs_1, self.f, self.m, self.n)
         amplitude_scaling_two_body(self.rhs_2, self.f, self.m, self.n)
 
@@ -205,6 +211,12 @@ class CoupledClusterSinglesDoubles(CoupledCluster):
         self._compute_lambda_intermediates()
         self._compute_ccsd_lambda_amplitudes_s()
         self._compute_ccsd_lambda_amplitudes_d()
+
+        if not iterative:
+            np.copyto(self.l_1, self.rhs_1_lambda)
+            np.copyto(self.l_2, self.rhs_2_lambda)
+
+            return
 
         amplitude_scaling_one_body_lambda(
             self.rhs_1_lambda, self.f, self.m, self.n
