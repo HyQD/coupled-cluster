0f735b083d5fd80e9fa71a1aec38fcc20b8d6bde
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-10-04 11:21:24 +0200

    Run larger system

--- scripts/test_2.py
+++ scripts/test_2.py
@@ -4,10 +4,10 @@ from coupled_cluster.ccsd import CoupledClusterSinglesDoubles
 import matplotlib.pyplot as plt
 import numpy as np
 
-n = 2
-l = 6
+n = 4
+l = 30
 length = 10
-num_grid_points = 1000
+num_grid_points = 400
 omega = 0.25
 laser_frequency = 8 * omega
 laser_strength = 1
@@ -33,12 +33,10 @@ ccsd.compute_lambda_amplitudes(theta=theta, tol=tol)
 rho = ccsd.compute_one_body_density()
 
 plt.plot(odho.grid, rho)
-plt.axis([-length, length, 0, 0.4])
 plt.show()
 
 t_start = 0
 t_end = 10
-# t_end = 4 * (2 * np.pi) / laser_frequency
 num_timesteps = 10001
 print((t_end - t_start) / (num_timesteps - 1))
 
