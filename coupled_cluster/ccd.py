2c8f92537de171dd581749bf2bdb2b875780966f
Mint-kontor <oyvindschoyen@gmail.com>
2018-10-13 15:21:50 +0200

    Fix wrong contraction and remove tests

--- coupled_cluster/ccd.py
+++ coupled_cluster/ccd.py
@@ -3,7 +3,6 @@ from .cc import CoupledCluster
 from .cc_helper import amplitude_scaling_two_body
 
 
-# TODO: The energy is too low, which means there is an error somewhere...
 class CoupledClusterDoubles(CoupledCluster):
     def __init__(self, system, **kwargs):
         super().__init__(system, **kwargs)
@@ -45,14 +44,6 @@ class CoupledClusterDoubles(CoupledCluster):
         energy = 0.25 * np.einsum("abij, abij ->", self.u[v, v, o, o], self.t_2)
         energy += self.compute_reference_energy()
 
-        # e_ref = np.einsum("ii ->", self.f[o, o])
-        # e_ref -= 0.5 * np.einsum("ijij ->", self.u[o, o, o, o])
-        # e_ref += 0.25 * np.einsum("abij, abij ->", self.u[v, v, o, o], self.t_2)
-        # np.testing.assert_allclose(
-        #    energy,
-        #    e_ref
-        # )
-
         return energy
 
     def _compute_amplitudes(self, theta, iterative=True):
@@ -78,90 +69,30 @@ class CoupledClusterDoubles(CoupledCluster):
 
         term = np.tensordot(f[v, v], self.t_2, axes=((1), (1)))
         term -= term.swapaxes(0, 1)
-
-        # term_test = np.einsum("bc, acij -> abij", f[v, v], self.t_2)
-        # term_test -= term_test.swapaxes(0, 1)
-        # np.testing.assert_allclose(
-        #    term,
-        #    -term_test
-        # )
-
         self.rhs_2 -= term
 
         term = np.tensordot(self.t_2, f[o, o], axes=((3), (0)))
         term -= term.swapaxes(2, 3)
-
-        # term_test = np.einsum("kj, abik -> abij", f[o, o], self.t_2)
-        # term_test -= term_test.swapaxes(2, 3)
-        # np.testing.assert_allclose(
-        #    term,
-        #    term_test
-        # )
-
         self.rhs_2 -= term
 
         self.rhs_2 += np.tensordot(self.W_pppp, self.t_2, axes=((2, 3), (0, 1)))
-        # term = np.tensordot(self.W_pppp, self.t_2, axes=((2, 3), (0, 1)))
-        # term_test = np.einsum(
-        #    "cdij, abcd -> abij", self.t_2, self.W_pppp
-        # )
-        # np.testing.assert_allclose(
-        #    term,
-        #    term_test
-        # )
-        # self.rhs_2 += term
 
         term = np.tensordot(self.t_2, self.W_hh, axes=((3), (0)))
         term -= term.swapaxes(2, 3)
-
-        # term_test = np.einsum(
-        #    "abin, nj -> abij", self.t_2, self.W_hh
-        # )
-        # term_test -= term_test.swapaxes(2, 3)
-        # np.testing.assert_allclose(
-        #    term,
-        #    term_test
-        # )
-
         self.rhs_2 += term
 
         term = np.tensordot(self.W_pp, self.t_2, axes=((1), (1)))
         term -= term.swapaxes(0, 1)
-
-        # term_test = np.einsum("bdij, ad -> abij", self.t_2, self.W_pp)
-        # term_test -= term_test.swapaxes(0, 1)
-        # np.testing.assert_allclose(
-        #    term,
-        #    term_test
-        # )
-
         self.rhs_2 -= term
 
         term = np.einsum("acim, bmjc -> abij", self.t_2, self.W_phhp)
         term -= term.swapaxes(0, 1)
         term -= term.swapaxes(2, 3)
-
-        # term_test = np.einsum("acim, bmjc -> abij", self.t_2, self.W_phhp)
-        # term_test -= term_test.swapaxes(0, 1)
-        # term_test -= term_test.swapaxes(2, 3)
-        # np.testing.assert_allclose(
-        #    term,
-        #    term_test
-        # )
-
         self.rhs_2 += term
 
-        term = np.einsum("mnjn -> mj", self.u[o, o, o, o])
-        term = 0.5 * np.tensordot(self.t_2, term, axes=((3), (0)))
-
-        # term_test = 0.5 * np.einsum(
-        #    "abim, mnjn -> abij", self.t_2, self.u[o, o, o, o]
-        # )
-        # np.testing.assert_allclose(
-        #    term,
-        #    term_test
-        # )
-
+        term = 0.5 * np.tensordot(
+            self.t_2, self.u[o, o, o, o], axes=((2, 3), (0, 1))
+        )
         self.rhs_2 += term
 
     def _compute_intermediates(self):
@@ -172,13 +103,6 @@ class CoupledClusterDoubles(CoupledCluster):
             self.t_2, self.u[o, o, v, v], axes=((2, 3), (0, 1))
         )
         self.W_pppp += 0.5 * self.u[v, v, v, v]
-        # np.testing.assert_allclose(
-        #    self.W_pppp,
-        #    0.5 * self.u[v, v, v, v]
-        #    + 0.25 * np.einsum(
-        #        "abmn, mncd -> abcd", self.t_2, self.u[o, o, v, v]
-        #    )
-        # )
 
         self.W_phhp.fill(0)
         self.W_phhp += self.u[v, o, o, v]
@@ -190,20 +114,8 @@ class CoupledClusterDoubles(CoupledCluster):
         self.W_pp += 0.5 * np.tensordot(
             self.t_2, self.u[o, o, v, v], axes=((1, 2, 3), (2, 0, 1))
         )
-        # np.testing.assert_allclose(
-        #    self.W_pp,
-        #    0.5 * np.einsum(
-        #        "acnm, nmcd -> ad", self.t_2, self.u[o, o, v, v]
-        #    )
-        # )
 
         self.W_hh.fill(0)
         self.W_hh += 0.5 * np.tensordot(
             self.u[o, o, v, v], self.t_2, axes=((0, 2, 3), (3, 0, 1))
         )
-        # np.testing.assert_allclose(
-        #    self.W_hh,
-        #    0.5 * np.einsum(
-        #        "cdjm, mncd -> nj", self.t_2, self.u[o, o, v, v]
-        #    )
-        # )
