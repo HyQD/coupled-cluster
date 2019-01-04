deaf53129c008100bf7a06413d4d220f835b0189
Schoyen-Linux <oyvinssc@student.matnat.uio.no>
2019-01-04 12:49:00 +0100

    Remove need for build in setup

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
 from setuptools import setup, find_packages
-from setuptools.extension import Extension
-from Cython.Build import cythonize
 
-import numpy as np
-import os
-import glob
-
-
-base_path = ["coupled_cluster"]
-
-source_files = [*glob.glob(os.path.join(*base_path, "*.pyx"))]
-
-include_dirs = [np.get_include()]
-
-extensions = [
-    Extension(
-        name="coupled_cluster.cc_helper",
-        sources=source_files,
-        language="c",
-        include_dirs=include_dirs,
-        extra_compile_args=["-fopenmp"],
-        extra_link_args=["-fopenmp"],
-    )
-]
-
-setup(
-    name="Coupled cluster",
-    version="0.0.1",
-    packages=find_packages(),
-    ext_modules=cythonize(extensions),
-)
+setup(name="Coupled cluster", version="0.0.1", packages=find_packages())
