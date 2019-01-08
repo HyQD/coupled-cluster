1a71282db5cc93788d33c0cc77058ed06960d8cb
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-08 12:02:14 +0100

    Refactor amplitude equations in CCD
    	This includes tests for each amplitude term and labelling of the
    	terms according to Shavitt and Bartlett.

---
+++ coupled_cluster/ccd/rhs.py
@@ -0,0 +1,347 @@
+# Labelling of the different terms comes from the book "Many-Body Methods in
+# Chemistry and Physics" by I. Shavitt and R. J. Bartlett. Note that the sign of
+# the diagrams below may differ from Shavitt and Bartlett, due to the
+# autogeneration by SymPy.
+#
+# Furthermore, due to the use of intermediates, some diagrams are combined.
+
+
+def compute_t_2_amplitude(f, u, t, o, v, out=None, np=None):
+    if np is None:
+        import numpy as np
+
+    if out is None:
+        out = np.zeros_like(t)
+
+    ## Construct intermediates
+    # W_pppp = construct_pppp_intermediate(u, t, o, v, np=np)
+    # W_hh = construct_hh_intermediate(u, t, o, v, np=np)
+    # W_pp = construct_pp_intermediate(u, t, o, v, np=np)
+    # W_phhp = construct_phhp_intermediate(u, t, o, v, np=np)
+
+    add_d1(u, o, v, out, np=np)
+    add_d2a(f, t, o, v, out, np=np)
+    add_d2b(f, t, o, v, out, np=np)
+    add_d2c(u, t, o, v, out, np=np)
+    add_d2d(u, t, o, v, out, np=np)
+    add_d2e(u, t, o, v, out, np=np)
+    add_d3a(u, t, o, v, out, np=np)
+    add_d3b(u, t, o, v, out, np=np)
+    add_d3c(u, t, o, v, out, np=np)
+    add_d3d(u, t, o, v, out, np=np)
+    # add_d2c_and_d3a(t, W_pppp, o, v, out, np=np)
+    # add_d3c(u, t, W_hh, o, v, out, np=np)
+    # add_d2e_and_d3b(u, t, W_phhp, o, v, out, np=np)
+    # add_d3d(t, W_pp, o, v, out, np=np)
+
+    return out
+
+
+# def construct_pppp_intermediate(u, t, o, v, np=None):
+#    """Function constructing the intermediate
+#
+#        W^{ab}_{cd} = 0.25 * t^{ab}_{kl} u^{kl}_{cd} + 0.5 * u^{ab}_{cd}
+#
+#    Number of FLOPS required: O(m^4 n^2).
+#    """
+#    if np is None:
+#        import numpy as np
+#
+#    W_pppp = 0.25 * np.tensordot(t, u[o, o, v, v], axes=((2, 3), (0, 1)))
+#    W_pppp += 0.5 * u[v, v, v, v]
+#
+#    return W_pppp
+#
+#
+# def construct_hh_intermediate(u, t, o, v, np=None):
+#    """Function constructing the intermediate
+#
+#        W^{l}_{j} = 0.5 * t^{cd}_{jk} u^{kl}_{cd}
+#
+#    Number of FLOPS required: O(m^2 n^3).
+#    """
+#    if np is None:
+#        import numpy as np
+#
+#    W_hh = 0.5 * np.tensordot(u[o, o, v, v], t, axes=((0, 2, 3), (3, 0, 1)))
+#
+#    return W_hh
+#
+#
+# def construct_pp_intermediate(u, t, o, v, np=None):
+#    """Function constructing the intermediate
+#
+#        W^{b}_{c} = 0.5 * t^{bd}_{kl} u^{kl}_{cd}
+#
+#    Number of FLOPS required: O(m^3 n^2).
+#    """
+#    if np is None:
+#        import numpy as np
+#
+#    W_pp = 0.5 * np.tensordot(t, u[o, o, v, v], axes=((1, 2, 3), (3, 0, 1)))
+#
+#    return W_pp
+#
+#
+# def construct_phhp_intermediate(u, t, o, v, np=None):
+#    """Function constructing the intermediate
+#
+#        W^{bk}_{jc} = 0.5 * t^{bd}_{jl} u^{kl}_{cd} + u^{bk}_{jc}
+#
+#    Number of FLOPS required: O(m^3 n^3).
+#    """
+#    if np is None:
+#        import numpy as np
+#
+#    W_phhp = 0.5 * np.einsum("bdjl, klcd -> bkjc", t, u[o, o, v, v])
+#    W_phhp += u[v, o, o, v]
+#
+#    return W_phhp
+
+
+def add_d1(u, o, v, out, np=None):
+    """Function adding the D1 diagram
+
+        g(f, u, t) <- u^{ab}_{ij}
+
+    Number of FLOPS required: O(m^2 n^2).
+    """
+    if np is None:
+        import numpy as np
+
+    out += u[v, v, o, o]
+
+
+def add_d2a(f, t, o, v, out, np=None):
+    """Function adding the D2a diagram
+
+        g(f, u, t) <- f^{b}_{c} t^{ac}_{ij} P(ab)
+
+    Number of FLOPS required: O(m^3 n^2).
+    """
+    if np is None:
+        import numpy as np
+
+    term = np.tensordot(f[v, v], t, axes=((1), (1))).transpose(1, 0, 2, 3)
+    term -= term.swapaxes(0, 1)
+    out += term
+
+
+def add_d2b(f, t, o, v, out, np=None):
+    """Function adding the D2b diagram
+
+        g(f, u, t) <- -f^{k}_{j} t^{ab}_{ik} P(ij)
+
+    Number of FLOPS required: O(m^2 n^3).
+    """
+    if np is None:
+        import numpy as np
+
+    term = np.tensordot(t, f[o, o], axes=((3), (0)))
+    term -= term.swapaxes(2, 3)
+    out -= term
+
+
+def add_d2c(u, t, o, v, out, np=None):
+    """Function adding the D2c diagram
+
+        g(f, u, t) <- 0.5 * t^{cd}_{ij} u^{ab}_{cd}
+
+    Number of FLOPS required: O(m^4 n^2).
+    """
+    if np is None:
+        import numpy as np
+
+    out += 0.5 * np.tensordot(u[v, v, v, v], t, axes=((2, 3), (0, 1)))
+
+
+def add_d2d(u, t, o, v, out, np=None):
+    """Function adding the D2d diagram
+
+        g(f, u, t) <- 0.5 * t^{ab}_{kl} u^{kl}_{ij}
+
+    Number of FLOPS required: O(m^2 n^4).
+    """
+    if np is None:
+        import numpy as np
+
+    out += 0.5 * np.tensordot(t, u[o, o, o, o], axes=((2, 3), (0, 1)))
+
+
+def add_d2e(u, t, o, v, out, np=None):
+    """Function adding the D2e diagram
+
+        g(f, u, t) <- t^{ac}_{ik} u^{bk}_{jc} P(ab) P(ij)
+
+    Number of FLOPS required: O(m^3 n^3).
+    """
+    if np is None:
+        import numpy as np
+
+    term = np.tensordot(t, u[v, o, o, v], axes=((1, 3), (3, 1))).transpose(
+        0, 2, 1, 3
+    )
+    term -= term.swapaxes(0, 1)
+    term -= term.swapaxes(2, 3)
+    out += term
+
+
+def add_d3a(u, t, o, v, out, np=None):
+    """Function adding the D3a diagram
+
+        g(f, u, t) <- 0.25 * t^{cd}_{ij} t^{ab}_{kl} u^{kl}_{cd}
+
+    We do this in two steps and in one of two ways depending on the number of
+    occupied indices.
+
+    1) If half, or more, of the basis functions are occupied, we
+    precompute
+
+        W^{ab}_{cd} = 0.25 * t^{ab}_{kl} u^{kl}_{cd}
+        g(f, u, t) <- t^{cd}_{ij} W^{ab}_{cd}
+
+    Number of FLOPS required: O(m^4 n^2).
+
+    2) If less than half of the basis functions are occupied, we precompute
+
+        W^{kl}_{ij} = 0.25 * t^{cd}_{ij} u^{kl}_{cd}
+        g(f, u, t) <- t^{ab}_{kl} W^{kl}_{ij}
+
+    Number of FLOPS required: O(m^2 n^4).
+    """
+    if np is None:
+        import numpy as np
+
+    if o.stop >= v.stop // 2:
+        # Case 1
+        W_abcd = 0.25 * np.tensordot(t, u[o, o, v, v], axes=((2, 3), (0, 1)))
+        out += np.tensordot(W_abcd, t, axes=((2, 3), (0, 1)))
+    else:
+        # Case 2
+        W_klij = 0.25 * np.tensordot(u[o, o, v, v], t, axes=((2, 3), (0, 1)))
+        out += np.tensordot(t, W_klij, axes=((2, 3), (0, 1)))
+
+
+def add_d3b(u, t, o, v, out, np=None):
+    """Function adding the D3b diagram
+
+        g(f, u, t) <- t^{ac}_{ik} t^{bd}_{jl} u^{kl}_{cd} P(ij)
+
+    We do this in two steps
+
+        W^{bk}_{jc} = t^{bd}_{jl} u^{kl}_{cd}
+        g(f, u, t) <- t^{ac}_{ik} W^{bk}_{jc} P(ij)
+
+    Number of FLOPS required: O(m^3 n^3).
+    """
+    if np is None:
+        import numpy as np
+
+    W_bkjc = np.tensordot(t, u[o, o, v, v], axes=((1, 3), (3, 1))).transpose(
+        0, 2, 1, 3
+    )
+    term = np.tensordot(t, W_bkjc, axes=((1, 3), (3, 1))).transpose(0, 2, 1, 3)
+    term -= term.swapaxes(2, 3)
+    out += term
+
+
+def add_d3c(u, t, o, v, out, np=None):
+    """Function adding the D3c diagram
+
+        g(f, u, t) <- -0.5 t^{ab}_{lj} t^{dc}_{ik} u^{kl}_{cd} P(ij)
+
+    We do this in two steps
+
+        W^{l}_{i} = 0.5 * t^{dc}_{ik} u^{kl}_{cd}
+        g(f, u, t) <- - t^{ab}_{lj} W^{l}_{i} P(ij)
+
+    Number of FLOPS required: O(m^2 n^3).
+    """
+    if np is None:
+        import numpy as np
+
+    W_li = 0.5 * np.tensordot(u[o, o, v, v], t, axes=((0, 2, 3), (3, 1, 0)))
+    term = -np.tensordot(t, W_li, axes=((2), (0))).transpose(0, 1, 3, 2)
+    term -= term.swapaxes(2, 3)
+    out += term
+
+
+def add_d3d(u, t, o, v, out, np=None):
+    """Function adding the D3d diagram
+
+        g(f, u, t) <- -0.5 t^{ac}_{lk} t^{db}_{ij} u^{kl}_{cd} P(ab)
+
+    We do this in two steps
+
+        W^{a}_{d} = 0.5 * t^{ac}_{lk} u^{kl}_{cd}
+        g(f, u, t) <- - t^{db}_{ij} W^{a}_{d} P(ab)
+
+    Number of FLOPS required: O(m^3 n^2).
+    """
+    if np is None:
+        import numpy as np
+
+    W_ad = 0.5 * np.tensordot(t, u[o, o, v, v], axes=((1, 2, 3), (2, 1, 0)))
+    term = -np.tensordot(W_ad, t, axes=((1), (0)))
+    term -= term.swapaxes(0, 1)
+    out += term
+
+
+# def add_d2c_and_d3a(t, W_pppp, o, v, out, np=None):
+#    """Function adding the D2c and D3a diagrams (using an intermediate tensor)
+#
+#        g(f, u, t) <- t^{cd}_{ij} W^{ab}_{cd}
+#
+#    Number of FLOPS required: O(m^4 n^2).
+#    """
+#    if np is None:
+#        import numpy as np
+#
+#    out += np.tensordot(W_pppp, t, axes=((2, 3), (0, 1)))
+
+
+# def add_d3c(u, t, W_hh, o, v, out, np=None):
+#    """Function adding the D3c diagram (using an intermediate tensor)
+#
+#        g(f, u, t) <- t^{ab}_{il} W^{l}_{j} P(ij)
+#
+#    Number of FLOPS required: O(m^2 n^3).
+#    """
+#    if np is None:
+#        import numpy as np
+#
+#    term = np.tensordot(t, W_hh, axes=((3), (0)))
+#    term -= term.swapaxes(2, 3)
+#    out += term
+
+
+# def add_d2e_and_d3b(u, t, W_phhp, o, v, out, np=None):
+#    """Function adding the D2e and D3b diagrams (using an intermediate tensor)
+#
+#        g(f, u, t) <- t^{ac}_{ik} W^{bk}_{jc} P(ab) P(ij)
+#
+#    Number of FLOPS required: O(m^3 n^3).
+#    """
+#    if np is None:
+#        import numpy as np
+#
+#    #term = np.einsum("acik, bkjc -> abij", t, W_phhp)
+#    term = np.tensordot(t, W_phhp, axes=((1, 3), (3, 1))).transpose(0, 2, 1, 3)
+#    term -= term.swapaxes(0, 1)
+#    term -= term.swapaxes(2, 3)
+#    out += term
+
+
+# def add_d3d(t, W_pp, o, v, out, np=None):
+#    """Function adding the D3d diagram (using an intermediate tensor)
+#
+#        g(f, u, t) <- -t^{ac}_{ij} W^{b}_{c} P(ab)
+#
+#    Number of FLOPS required: O(m^3 n^2).
+#    """
+#    if np is None:
+#        import numpy as np
+#
+#    term = np.tensordot(W_pp, t, axes=((1), (1)))
+#    term -= term.swapaxes(0, 1)
+#    out += term
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,29 @@
 import pytest
 import numpy as np
+import numba
 
-from quantum_systems import TwoDimensionalHarmonicOscillator
+from quantum_systems import TwoDimensionalHarmonicOscillator, CustomSystem
 
 l = 12  # Number of orbitals
 n = 2  # Number of particles
+n_large = 2
+l_large = 5
 
 radius = 4
 num_grid_points = 101
+
+
+@numba.njit(cache=True)
+def anti_symmetrize_t(t, m, n):
+    for a in range(m):
+        for b in range(a, m):
+            for i in range(n):
+                for j in range(i, n):
+                    t[a, b, j, i] = -t[a, b, i, j]
+                    t[b, a, i, j] = -t[a, b, i, j]
+                    t[b, a, j, i] = t[a, b, i, j]
+
+    return t
 
 
 @pytest.fixture(params=[0.5, 1.0])
@@ -22,6 +38,32 @@ def tdho(_omega):
     )
     _tdho.setup_system()
     return _tdho
+
+
+@pytest.fixture(scope="session")
+def large_system():
+    n = n_large
+    l = l_large
+
+    cs = CustomSystem(n, l)
+    cs.set_h(np.random.random((l, l)), add_spin=True)
+    cs.set_u(
+        np.random.random((l, l, l, l)), add_spin=True, anti_symmetrize=True
+    )
+    cs.construct_fock_matrix()
+
+    return cs
+
+
+@pytest.fixture(scope="session")
+def large_t():
+    n = n_large
+    m = l_large - n
+
+    t = np.random.random((m, m, n, n)).astype(np.complex128)
+    t = anti_symmetrize_t(t, m, n)
+
+    return t
 
 
 @pytest.fixture
