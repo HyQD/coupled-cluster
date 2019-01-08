7222e65710661246a60bb73fe074f67873f8990c
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-08 17:52:29 +0100

    Add tests of lambda diagrams

--- tests/test_ccd.py
+++ tests/test_ccd.py
@@ -25,11 +25,13 @@ from coupled_cluster.ccd.rhs_l import (
     add_d3c_l,
     add_d3d_l,
     add_d3e_l,
+    add_d3f_l,
+    add_d3g_l,
 )
 
 
 def test_add_d1_t(large_system):
-    t, large_system = large_system
+    t, l, large_system = large_system
     u = large_system.u
     o = large_system.o
     v = large_system.v
@@ -41,7 +43,7 @@ def test_add_d1_t(large_system):
 
 
 def test_add_d2a_t(large_system):
-    t, large_system = large_system
+    t, l, large_system = large_system
     f = large_system.f
     o = large_system.o
     v = large_system.v
@@ -55,7 +57,7 @@ def test_add_d2a_t(large_system):
 
 
 def test_add_d2b_t(large_system):
-    t, large_system = large_system
+    t, l, large_system = large_system
     f = large_system.f
     o = large_system.o
     v = large_system.v
@@ -69,7 +71,7 @@ def test_add_d2b_t(large_system):
 
 
 def test_add_d2c_t(large_system):
-    t, large_system = large_system
+    t, l, large_system = large_system
     u = large_system.u
     o = large_system.o
     v = large_system.v
@@ -82,7 +84,7 @@ def test_add_d2c_t(large_system):
 
 
 def test_add_d2d_t(large_system):
-    t, large_system = large_system
+    t, l, large_system = large_system
     u = large_system.u
     o = large_system.o
     v = large_system.v
@@ -95,7 +97,7 @@ def test_add_d2d_t(large_system):
 
 
 def test_add_d2e_t(large_system):
-    t, large_system = large_system
+    t, l, large_system = large_system
     u = large_system.u
     o = large_system.o
     v = large_system.v
@@ -110,7 +112,7 @@ def test_add_d2e_t(large_system):
 
 
 def test_add_d3a_t(large_system):
-    t, large_system = large_system
+    t, l, large_system = large_system
     u = large_system.u
     o = large_system.o
     v = large_system.v
@@ -125,7 +127,7 @@ def test_add_d3a_t(large_system):
 
 
 def test_add_d3b_t(large_system):
-    t, large_system = large_system
+    t, l, large_system = large_system
     u = large_system.u
     o = large_system.o
     v = large_system.v
@@ -141,7 +143,7 @@ def test_add_d3b_t(large_system):
 
 
 def test_add_d3c_t(large_system):
-    t, large_system = large_system
+    t, l, large_system = large_system
     u = large_system.u
     o = large_system.o
     v = large_system.v
@@ -157,7 +159,7 @@ def test_add_d3c_t(large_system):
 
 
 def test_add_d3d_t(large_system):
-    t, large_system = large_system
+    t, l, large_system = large_system
     u = large_system.u
     o = large_system.o
     v = large_system.v
@@ -168,6 +170,199 @@ def test_add_d3d_t(large_system):
         "aclk, dbij, klcd -> abij", t, t, u[o, o, v, v], optimize=True
     )
     out_e -= out_e.swapaxes(0, 1)
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d1_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d1_l(u, o, v, out, np=np)
+    out_e = u[o, o, v, v].copy()
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d2a_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d2a_l(u, l, o, v, out, np=np)
+    out_e = 0.5 * np.einsum("klab, ijkl -> ijab", l, u[o, o, o, o])
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d2b_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d2b_l(u, l, o, v, out, np=np)
+    out_e = 0.5 * np.einsum("ijdc, dcab -> ijab", l, u[v, v, v, v])
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d2c_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d2c_l(u, l, o, v, out, np=np)
+    out_e = -np.einsum("ijbc, ckak -> ijab", l, u[v, o, v, o])
+    out_e -= out_e.swapaxes(2, 3)
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d2d_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d2d_l(u, l, o, v, out, np=np)
+    out_e = np.einsum("jkab, ilkl -> ijab", l, u[o, o, o, o])
+    out_e -= out_e.swapaxes(0, 1)
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d2e_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d2e_l(u, l, o, v, out, np=np)
+    out_e = np.einsum("kjbc, icak -> ijab", l, u[o, v, v, o])
+    out_e -= out_e.swapaxes(0, 1)
+    out_e -= out_e.swapaxes(2, 3)
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d3a_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d3a_l(u, t, l, o, v, out, np=np)
+    out_e = -0.5 * np.einsum(
+        "ijbc, dckl, klad -> ijab", l, t, u[o, o, v, v], optimize=True
+    )
+    out_e -= out_e.swapaxes(2, 3)
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d3b_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d3b_l(u, t, l, o, v, out, np=np)
+    out_e = 0.25 * np.einsum(
+        "ijdc, dckl, klab -> ijab", l, t, u[o, o, v, v], optimize=True
+    )
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d3c_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d3c_l(u, t, l, o, v, out, np=np)
+    out_e = 0.5 * np.einsum(
+        "jkab, dckl, ildc -> ijab", l, t, u[o, o, v, v], optimize=True
+    )
+    out_e -= out_e.swapaxes(0, 1)
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d3d_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d3d_l(u, t, l, o, v, out, np=np)
+    out_e = -np.einsum(
+        "jkbc, dckl, ilad -> ijab", l, t, u[o, o, v, v], optimize=True
+    )
+    out_e -= out_e.swapaxes(0, 1)
+    out_e -= out_e.swapaxes(2, 3)
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d3e_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d3e_l(u, t, l, o, v, out, np=np)
+    out_e = 0.5 * np.einsum(
+        "jkdc, dckl, ilab -> ijab", l, t, u[o, o, v, v], optimize=True
+    )
+    out_e -= out_e.swapaxes(0, 1)
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d3f_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d3f_l(u, t, l, o, v, out, np=np)
+    out_e = 0.25 * np.einsum(
+        "klab, dckl, ijdc -> ijab", l, t, u[o, o, v, v], optimize=True
+    )
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+
+def test_add_d3g_l(large_system):
+    t, l, large_system = large_system
+    u = large_system.u
+    o = large_system.o
+    v = large_system.v
+
+    out = np.zeros_like(l)
+    add_d3g_l(u, t, l, o, v, out, np=np)
+    out_e = -0.5 * np.einsum(
+        "klbc, dckl, ijad -> ijab", l, t, u[o, o, v, v], optimize=True
+    )
+    out_e -= out_e.swapaxes(2, 3)
 
     np.testing.assert_allclose(out, out_e, atol=1e-10)
 
