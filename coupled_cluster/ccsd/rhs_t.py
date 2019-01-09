d91f81e72f30ea4cbf1a800acb22cc22309eb46f
gregwinther <sebastwi@uio.no>
2019-01-09 14:07:36 +0100

    Add s1_t, s2a_t, s2b_t functions incl. tests.

--- coupled_cluster/ccsd/rhs_t.py
+++ coupled_cluster/ccsd/rhs_t.py
@@ -20,3 +20,44 @@ def compute_t_2_amplitudes(f, u, t_1, t_2, o, v, out=None, np=None):
         out = np.zeros_like(t_2)
 
     return out
+
+
+def add_s1_t(f, o, v, out, np=None):
+    """Function adding the S1 diagram
+
+        g(f, u, t) <- f^{a}_{i}
+
+    Number of FLOPS required: O(m n).
+    """
+    if np is None:
+        import numpy as np
+
+    out += f[v, o]
+
+
+def add_s2a_t(f, t_2, o, v, out, np=None):
+    """Function adding the S2a diagram
+
+        g(f, u, t) <- f^{k}_{c} t^{ac}_{ik}
+
+    Numer of FLOPS required: O(m^2 n^2).
+    """
+
+    if np is None:
+        import Numpy as np
+
+    out += np.tensordot(f[o, v], t_2, axes=((0, 1), (3, 1)))
+
+
+def add_s2b_t(u, t_2, o, v, out, np=None):
+    """Function adding the S2b diagram
+
+        g(f, u, t) <- 0.5 u^{ak}_{cd} t^{cd}_{ik}
+
+    Number of FLOPS required: O(m^3 n^2).
+    """
+
+    if np is None:
+        import Numpy as np
+
+    out += 0.5 * np.tensordot(u[v, o, v, v], t_2, axes=((1, 2, 3), (3, 0, 1)))
--- tests/test_ccsd.py
+++ tests/test_ccsd.py
@@ -2,7 +2,44 @@ import pytest
 import numpy as np
 
 from coupled_cluster.ccsd import CoupledClusterSinglesDoubles
+from coupled_cluster.ccsd.rhs_t import add_s1_t, add_s2a_t, add_s2b_t
 
+
+def test_add_s1_t(large_system_ccsd):
+    t_1, t_2, l_1, l_2, cs = large_system_ccsd
+    f = cs.f
+    o = cs.o
+    v = cs.v
+
+    out = np.zeros_like(t_1)
+    add_s1_t(f, o, v, out, np=np)
+
+    np.testing.assert_allclose(out, f[v, o], atol=1e-10)
+
+
+def test_add_s2a_t(large_system_ccsd):
+    t_1, t_2, l_1, l_2, cs = large_system_ccsd
+    f = cs.f
+    o = cs.o
+    v = cs.v
+
+    out = np.zeros_like(t_1)
+    add_s2a_t(f, t_2, o, v, out, np=np)
+    out_e = np.einsum("kc, acik->ai", f[o, v], t_2)
+
+    np.testing.assert_allclose(out, out_e, atol=1e-10)
+
+def test_add_s2b_t(large_system_ccsd):
+    t_1, t_2, l_1, l_2, cs = large_system_ccsd
+    u = cs.u
+    o = cs.o
+    v = cs.v
+
+    out = np.zeros_like(t_1)
+    add_s2b_t(u, t_2, o, v, out, np=np)
+    out_e = np.einsum("akcd, cdik->ai", u[v, o, v, v], t_2)
+
+    np.testing.assert_allclose(out, out_e, atol=1e10)
 
 def test_mbpt_enegy(tdho):
     tol = 1e-4
