62df2c02e6ef94e0c522ce1ed409e16485c83821
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-09 11:13:36 +0100

    Move ccsd to separate folder

---
+++ coupled_cluster/ccsd/__init__.py
@@ -0,0 +1,1 @@
+from .ccsd import CoupledClusterSinglesDoubles
--- coupled_cluster/ccsd/ccsd.py
+++ coupled_cluster/ccsd/ccsd.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .cc import CoupledCluster
+from coupled_cluster.cc import CoupledCluster
 
 
 class CoupledClusterSinglesDoubles(CoupledCluster):
