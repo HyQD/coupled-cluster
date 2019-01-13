5f49de464465de021621f28108a9689eba1cc2ae
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-13 21:00:01 +0100

    Add warning on summation indices

--- symbolic-expressions/one_body_density_matrix_ccsd.py
+++ symbolic-expressions/one_body_density_matrix_ccsd.py
@@ -4,5 +4,9 @@ from one_body_density_matrix import (
 )
 from sympy import latex
 
+
+# Note that the summation indices might not correspond to the labels in the
+# symbol list. This can be remedied by carefully repeated indices with
+# non-repeated indices until the labels properly match the expressions.
 for label, (p, q) in symbol_list:
     print(label + latex(get_ccsd_one_body_density_matrix(p=p, q=q)))
