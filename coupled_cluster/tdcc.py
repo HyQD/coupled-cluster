1baa8cb7b3a9b24e3ff2059530736a1b0e6881e1
Mint-kontor <oyvindschoyen@gmail.com>
2019-01-15 16:29:24 +0100

    Use system in tdcc

--- coupled_cluster/tdcc.py
+++ coupled_cluster/tdcc.py
@@ -3,11 +3,12 @@ from coupled_cluster.cc_helper import AmplitudeContainer
 
 
 class TimeDependentCoupledCluster:
-    def __init__(self, rhs_t_func, rhs_l_func, np=None):
+    def __init__(self, rhs_t_func, rhs_l_func, system, np=None):
         if np is None:
             import numpy as np
 
         self.np = np
+        self.system = system
 
         if not isinstance(rhs_t_func, collections.Iterable):
             rhs_t_func = [rhs_t_func]
@@ -18,20 +19,22 @@ class TimeDependentCoupledCluster:
         self.rhs_t_func = rhs_t_func
         self.rhs_l_func = rhs_l_func
 
-    def rhs(self, f, u, o, v, l, t, current_time):
-        # o, v = self.system.o, self.system.v
+    def rhs(self, prev_amp, current_time):
+        o, v = self.system.o, self.system.v
 
-        # h = self.system.h_t(current_time)
-        # u = self.system.u_t(current_time)
-        # f = self.system.construct_fock_matrix(h, u)
+        h = self.system.h_t(current_time)
+        u = self.system.u_t(current_time)
+        f = self.system.construct_fock_matrix(h, u)
+
+        l_old, t_old = prev_amp
 
         t_new = [
-            -1j * rhs_t_func(f, u, *t, o, v, np=self.np)
+            -1j * rhs_t_func(f, u, *t_old, o, v, np=self.np)
             for rhs_t_func in self.rhs_t_func
         ]
 
         l_new = [
-            1j * rhs_l_func(f, u, *t, *l, o, v, np=self.np)
+            1j * rhs_l_func(f, u, *t_old, *l_old, o, v, np=self.np)
             for rhs_l_func in self.rhs_l_func
         ]
 
