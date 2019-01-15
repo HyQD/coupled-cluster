7a743a2c2049f6fbcd5120035cf0423b9153ee2e
Mint-kontor <oyvindschoyen@gmail.com>
2019-01-15 16:30:46 +0100

    Add Runge-Kutta 4 step

--- coupled_cluster/tdcc.py
+++ coupled_cluster/tdcc.py
@@ -39,3 +39,14 @@ class TimeDependentCoupledCluster:
         ]
 
         return AmplitudeContainer(l=l_new, t=t_new)
+
+    def rk4_step(self, u, t, dt):
+        f = self.rhs
+
+        K1 = dt * f(u, t)
+        K2 = dt * f(u + 0.5 * K1, t + 0.5 * dt)
+        K3 = dt * f(u + 0.5 * K2, t + 0.5 * dt)
+        K4 = dt * f(u + K3, t + dt)
+        u_new = u + (1 / 6.0) * (K1 + 2 * K2 + 2 * K3 + K4)
+
+        return u_new
