1f3a6104ee811c3b8eb3dd87918f38343d66ffa5
Mint-kontor <oyvindschoyen@gmail.com>
2019-01-15 11:20:55 +0100

    Rename functions and avoid usage of system

--- coupled_cluster/tdcc.py
+++ coupled_cluster/tdcc.py
@@ -2,36 +2,36 @@ import collections
 
 
 class TimeDependentCoupledCluster:
-    def __init__(self, system, rhs_t, rhs_l, np=None):
-        self.system = self.system
-
+    def __init__(self, rhs_t_func, rhs_l_func, np=None):
         if np is None:
             import numpy as np
 
         self.np = np
 
-        if not isinstance(rhs_t, collections.Iterable):
-            rhs_t = [rhs_t]
+        if not isinstance(rhs_t_func, collections.Iterable):
+            rhs_t_func = [rhs_t_func]
 
-        if not isinstance(rhs_l, collections.Iterable):
-            rhs_l = [rhs_l]
+        if not isinstance(rhs_l_func, collections.Iterable):
+            rhs_l_func = [rhs_l_func]
 
-        self.rhs_t = rhs_t
-        self.rhs_l = rhs_l
+        self.rhs_t_func = rhs_t_func
+        self.rhs_l_func = rhs_l_func
 
-    def rhs(self, l, t, current_time):
-        o, v = self.system.o, self.system.v
+    def rhs(self, f, u, o, v, l, t, current_time):
+        # o, v = self.system.o, self.system.v
 
-        h = self.system.h_t(current_time)
-        u = self.system.u_t(current_time)
-        f = self.system.construct_fock_matrix(h, u)
+        # h = self.system.h_t(current_time)
+        # u = self.system.u_t(current_time)
+        # f = self.system.construct_fock_matrix(h, u)
 
         t_new = [
-            -1j * rhs_t(f, u, *t, o, v, np=self.np) for rhs_t in self.rhs_t
+            -1j * rhs_t_func(f, u, *t, o, v, np=self.np)
+            for rhs_t_func in self.rhs_t_func
         ]
 
         l_new = [
-            1j * rhs_l(f, u, *t, *l, o, v, np=self.np) for rhs_l in self.rhs_l
+            1j * rhs_l_func(f, u, *t, *l, o, v, np=self.np)
+            for rhs_l_func in self.rhs_l_func
         ]
 
         return l_new, t_new
