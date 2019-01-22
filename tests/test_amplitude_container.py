788d661adb8bbb04f939194f704bb393014b7267
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-22 13:13:42 +0100

    Reverse ordering of amplitudes in test

--- tests/test_amplitude_container.py
+++ tests/test_amplitude_container.py
@@ -5,24 +5,24 @@ from coupled_cluster.cc_helper import AmplitudeContainer
 def test_addition_single_amp(large_system_ccd):
     t, l, cs = large_system_ccd
 
-    container = AmplitudeContainer(l=l, t=t)
+    container = AmplitudeContainer(t=t, l=l)
     k = 10
     new_container = container + k
-    new_l, new_t = new_container
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t + k, new_t[0], atol=1e-10)
     np.testing.assert_allclose(l + k, new_l[0], atol=1e-10)
 
-    new_container = container + [[l], [t]]
-    new_l, new_t = new_container
+    new_container = container + [[t], [l]]
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t + t, new_t[0], atol=1e-10)
     np.testing.assert_allclose(l + l, new_l[0], atol=1e-10)
 
     new_container = container + container
-    new_l, new_t = new_container
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t + t, new_t[0], atol=1e-10)
@@ -32,24 +32,24 @@ def test_addition_single_amp(large_system_ccd):
 def test_multiplication_single_amp(large_system_ccd):
     t, l, cs = large_system_ccd
 
-    container = AmplitudeContainer(l=l, t=t)
+    container = AmplitudeContainer(t=t, l=l)
     k = 10
     new_container = container * k
-    new_l, new_t = new_container
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t * k, new_t[0], atol=1e-10)
     np.testing.assert_allclose(l * k, new_l[0], atol=1e-10)
 
-    new_container = container * [[l], [t]]
-    new_l, new_t = new_container
+    new_container = container * [[t], [l]]
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t * t, new_t[0], atol=1e-10)
     np.testing.assert_allclose(l * l, new_l[0], atol=1e-10)
 
     new_container = container * container
-    new_l, new_t = new_container
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t * t, new_t[0], atol=1e-10)
@@ -59,10 +59,10 @@ def test_multiplication_single_amp(large_system_ccd):
 def test_addition_double_amp(large_system_ccsd):
     t_1, t_2, l_1, l_2, cs = large_system_ccsd
 
-    container = AmplitudeContainer(l=[l_1, l_2], t=[t_1, t_2])
+    container = AmplitudeContainer(t=[t_1, t_2], l=[l_1, l_2])
     k = 10
     new_container = container + k
-    new_l, new_t = new_container
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t_1 + k, new_t[0], atol=1e-10)
@@ -70,8 +70,8 @@ def test_addition_double_amp(large_system_ccsd):
     np.testing.assert_allclose(l_1 + k, new_l[0], atol=1e-10)
     np.testing.assert_allclose(l_2 + k, new_l[1], atol=1e-10)
 
-    new_container = container + [[l_1, l_2], [t_1, t_2]]
-    new_l, new_t = new_container
+    new_container = container + [[t_1, t_2], [l_1, l_2]]
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t_1 + t_1, new_t[0], atol=1e-10)
@@ -80,7 +80,7 @@ def test_addition_double_amp(large_system_ccsd):
     np.testing.assert_allclose(l_2 + l_2, new_l[1], atol=1e-10)
 
     new_container = container + container
-    new_l, new_t = new_container
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     assert type(new_container) == type(container)
@@ -93,10 +93,10 @@ def test_addition_double_amp(large_system_ccsd):
 def test_addition_double_amp(large_system_ccsd):
     t_1, t_2, l_1, l_2, cs = large_system_ccsd
 
-    container = AmplitudeContainer(l=[l_1, l_2], t=[t_1, t_2])
+    container = AmplitudeContainer(t=[t_1, t_2], l=[l_1, l_2])
     k = 10
     new_container = container * k
-    new_l, new_t = new_container
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t_1 * k, new_t[0], atol=1e-10)
@@ -104,8 +104,8 @@ def test_addition_double_amp(large_system_ccsd):
     np.testing.assert_allclose(l_1 * k, new_l[0], atol=1e-10)
     np.testing.assert_allclose(l_2 * k, new_l[1], atol=1e-10)
 
-    new_container = container * [[l_1, l_2], [t_1, t_2]]
-    new_l, new_t = new_container
+    new_container = container * [[t_1, t_2], [l_1, l_2]]
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t_1 * t_1, new_t[0], atol=1e-10)
@@ -114,7 +114,7 @@ def test_addition_double_amp(large_system_ccsd):
     np.testing.assert_allclose(l_2 * l_2, new_l[1], atol=1e-10)
 
     new_container = container * container
-    new_l, new_t = new_container
+    new_t, new_l = new_container
 
     assert type(new_container) == type(container)
     assert type(new_container) == type(container)
