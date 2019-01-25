9044341db33b420babd4d20c3b23008b34db4e9b
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-25 16:01:11 +0100

    Add time-dependent observables in ccd

--- coupled_cluster/ccd/tdccd.py
+++ coupled_cluster/ccd/tdccd.py
@@ -1,9 +1,37 @@
 from coupled_cluster.tdcc import TimeDependentCoupledCluster
+from coupled_cluster.ccd.rhs_t import compute_t_2_amplitudes
+from coupled_cluster.ccd.rhs_l import compute_l_2_amplitudes
+from coupled_cluster.ccd.energies import compute_time_dependent_energy
+from coupled_cluster.ccd.density_matrices import compute_one_body_density_matrix
+from coupled_cluster.ccd.time_dependent_overlap import (
+    compute_time_dependent_overlap,
+)
 
 
-class TimeDependentCoupledClusterDoubles(TimeDependentCoupledClusterDoubles):
+class TimeDependentCoupledClusterDoubles(TimeDependentCoupledCluster):
     def rhs_t_amplitudes(self):
-        pass
+        yield compute_t_2_amplitudes
 
     def rhs_l_amplitudes(self):
-        pass
+        yield compute_l_2_amplitudes
+
+    def compute_energy(self):
+        t_2, l_2 = self._amplitudes
+
+        return compute_time_dependent_energy(
+            self.f, self.u, t_2, l_2, self.o, self.v, np=self.np
+        )
+
+    def compute_one_body_density_matrix(self):
+        t_2, l_2 = self._amplitudes
+
+        return compute_one_body_density_matrix(
+            t_2, l_2, self.system.o, self.system.v
+        )
+
+    def compute_time_dependent_overlap(self):
+        t_2, l_2 = self._amplitudes
+
+        return compute_time_dependent_overlap(
+            self.cc.t_2, self.cc.l_2, t_2, l_2, np=self.np
+        )
