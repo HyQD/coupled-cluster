c948dc11e447920d712f5c6e6c842a731123949e
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-25 15:58:58 +0100

    Remove commented code and add doc

--- coupled_cluster/cc.py
+++ coupled_cluster/cc.py
@@ -12,8 +12,8 @@ from coupled_cluster.cc_helper import (
 
 
 class CoupledCluster(metaclass=abc.ABCMeta):
-    """Abstract base class defining the skeleton of a Coupled Cluster solver
-    class.
+    """Abstract base class defining the skeleton of a Coupled Cluster ground
+    state solver class.
     """
 
     def __init__(self, system, verbose=False, np=None):
@@ -122,97 +122,3 @@ class CoupledCluster(metaclass=abc.ABCMeta):
             ]
 
             t_list = new_t_list
-
-    # def _timestep(self, l_i, t_i, time):
-    #    self._set_l(l_i)
-    #    self._set_t(t_i)
-
-    #    self.h = self.system.h_t(time)
-    #    self.u = self.system.u_t(time)
-    #    self.f = self.system.construct_fock_matrix(self.h, self.u)
-
-    #    t_new = [
-    #        -1j * t_x for t_x in self._compute_t_amplitudes(0, iterative=False)
-    #    ]
-    #    l_new = [
-    #        1j * l_x for l_x in self._compute_l_amplitudes(0, iterative=False)
-    #    ]
-
-    #    return (l_new, t_new)
-
-    # def evolve_amplitudes(self, t_start, t_end, num_timesteps):
-    #    prob = np.zeros(num_timesteps, dtype=np.complex128)
-    #    time = np.zeros(num_timesteps)
-
-    #    time[0] = t_start
-    #    h = (t_end - t_start) / (num_timesteps - 1)
-
-    #    self._l_0 = self._get_l_copy()
-    #    self._t_0 = self._get_t_copy()
-    #    prob[0] = self._compute_time_evolution_probability()
-
-    #    assert abs(prob[0].real - 1.0) < 1e-8
-
-    #    l_i = self._l_0
-    #    t_i = self._t_0
-    #    for i in tqdm.tqdm(range(1, num_timesteps)):
-    #        time[i] = i * h
-
-    #        k_1_l, k_1_t = self._timestep(l_i, t_i, time[i])
-
-    #        l_i2 = [l_x + h * k_1 / 2.0 for l_x, k_1 in zip(l_i, k_1_l)]
-    #        t_i2 = [t_x + h * k_1 / 2.0 for t_x, k_1 in zip(t_i, k_1_t)]
-    #        k_2_l, k_2_t = self._timestep(l_i2, t_i2, time[i] + h / 2.0)
-
-    #        l_i3 = [l_x + h * k_2 / 2.0 for l_x, k_2 in zip(l_i, k_2_l)]
-    #        t_i3 = [t_x + h * k_2 / 2.0 for t_x, k_2 in zip(t_i, k_2_t)]
-    #        k_3_l, k_3_t = self._timestep(l_i3, t_i3, time[i] + h / 2.0)
-
-    #        l_i4 = [l_x + h * k_3 for l_x, k_3 in zip(l_i, k_3_l)]
-    #        t_i4 = [t_x + h * k_3 for t_x, k_3 in zip(t_i, k_3_t)]
-    #        k_4_l, k_4_t = self._timestep(l_i4, t_i4, time[i] + h)
-
-    #        l_i = [
-    #            l_x + h / 6.0 * (k_1 + 2 * k_2 + 2 * k_3 + k_4)
-    #            for l_x, k_1, k_2, k_3, k_4 in zip(
-    #                l_i, k_1_l, k_2_l, k_3_l, k_4_l
-    #            )
-    #        ]
-    #        t_i = [
-    #            t_x + h / 6.0 * (k_1 + 2 * k_2 + 2 * k_3 + k_4)
-    #            for t_x, k_1, k_2, k_3, k_4 in zip(
-    #                t_i, k_1_t, k_2_t, k_3_t, k_4_t
-    #            )
-    #        ]
-
-    #        self._set_l(l_i)
-    #        self._set_t(t_i)
-
-    #        prob[i] = self._compute_time_evolution_probability()
-
-    #    return prob, time
-
-    # def __err(self, func_name):
-    #    raise NotImplementedError(
-    #        "Class '{0}' does not have an implementation of '{1}'".format(
-    #            self.__class__.__name__, func_name
-    #        )
-    #    )
-
-    # def _compute_time_overlap_with_ground_state(self):
-    #    self.__err(self._compute_time_overlap_with_ground_state.__name__)
-
-    # @abc.abstractmethod
-    # def _set_t(self, t):
-    #    pass
-
-    # def _set_l(self, l):
-    #    self.__err(self._set_l.__name__)
-
-    # @abc.abstractmethod
-    # def _get_t_copy(self):
-    #    pass
-
-    # @abc.abstractmethod
-    # def _get_l_copy(self):
-    #    pass
