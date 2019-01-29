1c6d62996d8616bb19d0b7a05330f50914852735
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-29 21:07:49 +0100

    Add test of OACCVector

--- tests/test_amplitude_container.py
+++ tests/test_amplitude_container.py
@@ -1,5 +1,5 @@
 import numpy as np
-from coupled_cluster.cc_helper import AmplitudeContainer
+from coupled_cluster.cc_helper import AmplitudeContainer, OACCVector
 
 
 def test_addition_single_amp(large_system_ccd):
@@ -83,7 +83,6 @@ def test_addition_double_amp(large_system_ccsd):
     new_t, new_l = new_container
 
     assert type(new_container) == type(container)
-    assert type(new_container) == type(container)
     np.testing.assert_allclose(t_1 + t_1, new_t[0], atol=1e-10)
     np.testing.assert_allclose(t_2 + t_2, new_t[1], atol=1e-10)
     np.testing.assert_allclose(l_1 + l_1, new_l[0], atol=1e-10)
@@ -117,8 +116,89 @@ def test_multiplication_double_amp(large_system_ccsd):
     new_t, new_l = new_container
 
     assert type(new_container) == type(container)
+    np.testing.assert_allclose(t_1 * t_1, new_t[0], atol=1e-10)
+    np.testing.assert_allclose(t_2 * t_2, new_t[1], atol=1e-10)
+    np.testing.assert_allclose(l_1 * l_1, new_l[0], atol=1e-10)
+    np.testing.assert_allclose(l_2 * l_2, new_l[1], atol=1e-10)
+
+
+def test_addition_double_amp_oaccvector(large_system_ccsd):
+    t_1, t_2, l_1, l_2, cs = large_system_ccsd
+    C = np.random.random((cs.l, cs.l)).astype(t_2.dtype)
+    C_tilde = np.random.random((cs.l, cs.l)).astype(t_2.dtype)
+
+    container = OACCVector(t=[t_1, t_2], l=[l_1, l_2], C=C, C_tilde=C_tilde)
+    k = 10
+    new_container = container + k
+    new_t, new_l, new_C, new_C_tilde = new_container
+
+    assert type(new_container) == type(container)
+    np.testing.assert_allclose(t_1 + k, new_t[0], atol=1e-10)
+    np.testing.assert_allclose(t_2 + k, new_t[1], atol=1e-10)
+    np.testing.assert_allclose(l_1 + k, new_l[0], atol=1e-10)
+    np.testing.assert_allclose(l_2 + k, new_l[1], atol=1e-10)
+    np.testing.assert_allclose(C + k, new_C, atol=1e-10)
+    np.testing.assert_allclose(C_tilde + k, new_C_tilde, atol=1e-10)
+
+    new_container = container + [[t_1, t_2], [l_1, l_2], C, C_tilde]
+    new_t, new_l, new_C, new_C_tilde = new_container
+
+    assert type(new_container) == type(container)
+    np.testing.assert_allclose(t_1 + t_1, new_t[0], atol=1e-10)
+    np.testing.assert_allclose(t_2 + t_2, new_t[1], atol=1e-10)
+    np.testing.assert_allclose(l_1 + l_1, new_l[0], atol=1e-10)
+    np.testing.assert_allclose(l_2 + l_2, new_l[1], atol=1e-10)
+    np.testing.assert_allclose(C + C, new_C, atol=1e-10)
+    np.testing.assert_allclose(C_tilde + C_tilde, new_C_tilde, atol=1e-10)
+
+    new_container = container + container
+    new_t, new_l, new_C, new_C_tilde = new_container
+
+    assert type(new_container) == type(container)
+    np.testing.assert_allclose(t_1 + t_1, new_t[0], atol=1e-10)
+    np.testing.assert_allclose(t_2 + t_2, new_t[1], atol=1e-10)
+    np.testing.assert_allclose(l_1 + l_1, new_l[0], atol=1e-10)
+    np.testing.assert_allclose(l_2 + l_2, new_l[1], atol=1e-10)
+    np.testing.assert_allclose(C + C, new_C, atol=1e-10)
+    np.testing.assert_allclose(C_tilde + C_tilde, new_C_tilde, atol=1e-10)
+
+
+def test_multiplication_double_amp_oaccvector(large_system_ccsd):
+    t_1, t_2, l_1, l_2, cs = large_system_ccsd
+    C = np.random.random((cs.l, cs.l)).astype(t_2.dtype)
+    C_tilde = np.random.random((cs.l, cs.l)).astype(t_2.dtype)
+
+    container = OACCVector(t=[t_1, t_2], l=[l_1, l_2], C=C, C_tilde=C_tilde)
+    k = 10
+    new_container = container * k
+    new_t, new_l, new_C, new_C_tilde = new_container
+
+    assert type(new_container) == type(container)
+    np.testing.assert_allclose(t_1 * k, new_t[0], atol=1e-10)
+    np.testing.assert_allclose(t_2 * k, new_t[1], atol=1e-10)
+    np.testing.assert_allclose(l_1 * k, new_l[0], atol=1e-10)
+    np.testing.assert_allclose(l_2 * k, new_l[1], atol=1e-10)
+    np.testing.assert_allclose(C * k, new_C, atol=1e-10)
+    np.testing.assert_allclose(C_tilde * k, new_C_tilde, atol=1e-10)
+
+    new_container = container * [[t_1, t_2], [l_1, l_2], C, C_tilde]
+    new_t, new_l, new_C, new_C_tilde = new_container
+
     assert type(new_container) == type(container)
     np.testing.assert_allclose(t_1 * t_1, new_t[0], atol=1e-10)
     np.testing.assert_allclose(t_2 * t_2, new_t[1], atol=1e-10)
     np.testing.assert_allclose(l_1 * l_1, new_l[0], atol=1e-10)
     np.testing.assert_allclose(l_2 * l_2, new_l[1], atol=1e-10)
+    np.testing.assert_allclose(C * C, new_C, atol=1e-10)
+    np.testing.assert_allclose(C_tilde * C_tilde, new_C_tilde, atol=1e-10)
+
+    new_container = container * container
+    new_t, new_l, new_C, new_C_tilde = new_container
+
+    assert type(new_container) == type(container)
+    np.testing.assert_allclose(t_1 * t_1, new_t[0], atol=1e-10)
+    np.testing.assert_allclose(t_2 * t_2, new_t[1], atol=1e-10)
+    np.testing.assert_allclose(l_1 * l_1, new_l[0], atol=1e-10)
+    np.testing.assert_allclose(l_2 * l_2, new_l[1], atol=1e-10)
+    np.testing.assert_allclose(C * C, new_C, atol=1e-10)
+    np.testing.assert_allclose(C_tilde * C_tilde, new_C_tilde, atol=1e-10)
