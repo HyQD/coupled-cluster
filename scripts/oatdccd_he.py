60872773d8158852aca21d02406aca9e8ba7c5c0
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-30 14:25:40 +0100

    Add plot

--- scripts/oatdccd_he.py
+++ scripts/oatdccd_he.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import time
 from quantum_systems import construct_psi4_system
 from quantum_systems.time_evolution_operators import LaserField
@@ -34,7 +35,7 @@ symmetry c1
 
 options = {"basis": "cc-pvdz", "scf_type": "pk", "e_convergence": 1e-8}
 omega = 2.873_564_3
-E = 1  # 0.05-5
+E = 100  # 0.05-5
 
 
 system = construct_psi4_system(He, options)
@@ -55,14 +56,26 @@ system.set_polarization_vector(polarization)
 system.set_time_evolution_operator(LaserField(laser_pulse(omega=omega, E=E)))
 
 oatdccd.set_initial_conditions()
-time_points = np.linspace(0, 5, 1001)
+time_points = np.linspace(0, 2, 2001)
+dt = time_points[1] - time_points[0]
+print("dt = {0}".format(dt))
+td_energies = np.zeros(len(time_points))
+
+td_energies[0] = oatdccd.compute_energy().real
 
 for i, amp in enumerate(oatdccd.solve(time_points)):
     t, l, C, C_tilde = amp
+    td_energies[i + 1] = oatdccd.compute_energy().real
+    if i % 100 == 0:
+        print(f"i = {i}")
 
     eye = C_tilde @ C
-    print(eye)
-    print(np.diag(eye))
-    # np.testing.assert_allclose(C_tilde @ C, np.eye(C_tilde.shape[0]), atol=1e-7)
+    # print(eye)
+    # print(np.diag(eye))
+    # np.testing.assert_allclose(C_tilde @ C, np.eye(C_tilde.shape[0]), atol=1e-10)
     # if i == 1:
     #    break
+
+plt.plot(time_points, td_energies)
+plt.grid()
+plt.show()
