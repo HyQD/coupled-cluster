547371d2e5a61e04c2bfdcb677d8e1fc2c3cd189
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-31 19:35:49 +0100

    Save files after run

--- scripts/oatdccd_be.py
+++ scripts/oatdccd_be.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import os
 from quantum_systems import construct_psi4_system
 from quantum_systems.time_evolution_operators import LaserField
 from tdhf import HartreeFock
@@ -39,7 +40,7 @@ Use same laser parameters as in Thomas and Simen's article: https://arxiv.org/ab
 With static orbitals they only manage E=0.3 and the method breaks completely down with E=1.
 """
 
-omega = 0.2068175
+omega = 0.206_817_5
 E = 1  # E = 0.3, 1
 laser_duration = 5
 
@@ -60,30 +61,41 @@ print(
 polarization = np.zeros(3)
 polarization[2] = 1
 system.set_polarization_vector(polarization)
-system.set_time_evolution_operator(LaserField(laser_pulse(td=laser_duration, omega=omega, E=E)))
+system.set_time_evolution_operator(
+    LaserField(laser_pulse(td=laser_duration, omega=omega, E=E))
+)
 
 oatdccd.set_initial_conditions()
 
-dt = 1e-2
-Tfinal = 5
-Nsteps = int(Tfinal/dt) + 1
-timestep_stop_laser = int(laser_duration/dt)
+# dt = 1e-2
+# Tfinal = 5
+# Nsteps = int(Tfinal/dt) + 1
+# timestep_stop_laser = int(laser_duration/dt)
 
-time_points = np.linspace(0, Tfinal, Nsteps)
-print("Nsteps: %d" % Nsteps)
-print("step stop laser: %d" % timestep_stop_laser)
-print("dt = {0}".format(dt))
+t_final = 1000
+num_timesteps = 100_001
+time_points = np.linspace(0, t_final, num_timesteps)
+dt = time_points[1] - time_points[0]
+num_timesteps_with_laser = int(laser_duration / dt)
+
+print(f"Number of timesteps: {num_timesteps}")
+print(f"dt = {dt}")
+print(
+    f"Number of timesteps with the laser turned on: {num_timesteps_with_laser}"
+)
 
 
-td_energies = np.zeros(len(time_points))
-td_energies_imag = np.zeros(len(time_points))
-norm_t2 = np.zeros(len(time_points))
-norm_l2 = np.zeros(len(time_points))
-dip_z = np.zeros(len(time_points))
+td_energies = np.zeros(num_timesteps)
+td_energies_imag = np.zeros(num_timesteps)
+norm_t2 = np.zeros(num_timesteps)
+norm_l2 = np.zeros(num_timesteps)
+dip_z = np.zeros(num_timesteps)
 td_energies[0] = oatdccd.compute_energy()
 
-#norm_t2[0] = np.linalg.norm(oatdccd.t)
-#norm_l2[0] = np.linalg.norm(oatdccd.l)
+t, l, C, C_tilde = oatdccd.amplitudes
+
+norm_t2[0] = np.linalg.norm(t)
+norm_l2[0] = np.linalg.norm(l)
 
 
 for i, amp in enumerate(oatdccd.solve(time_points)):
@@ -105,62 +117,76 @@ for i, amp in enumerate(oatdccd.solve(time_points)):
     norm_t2[i + 1] = np.linalg.norm(t)
     norm_l2[i + 1] = np.linalg.norm(l)
 
-    if i % 5 == 0:
+    if i % 10 == 0:
         print(f"i = {i}")
         eye = C_tilde @ C
-        print(np.allclose(eye, np.eye(eye.shape[0]),atol=1e-4))
+        print(np.allclose(eye, np.eye(eye.shape[0]), atol=1e-4))
         print("norm(t2): %g" % np.linalg.norm(t))
         print("norm(l2): %g" % np.linalg.norm(l))
-    # print(eye)
-    # print(np.diag(eye))
-    # np.testing.assert_allclose(C_tilde @ C, np.eye(C_tilde.shape[0]), atol=1e-10)
-    # if i == 1:
-    #    break
+
+path = os.path.join(
+    "scripts", "Figures", "Beryllium", "E=1", "dt=1e-2", "long_run"
+)
 
 plt.figure()
 plt.plot(time_points, td_energies)
 plt.title("Time-dependent energy")
 plt.grid()
+plt.savefig(os.path.join(path, "td_energy.png"))
 
 plt.figure()
-plt.plot(time_points[time_points > 5], td_energies[time_points > 5])
+plt.plot(
+    time_points[time_points > laser_duration],
+    td_energies[time_points > laser_duration],
+)
 plt.title("Time-dependent energy, after laser is turned off")
 plt.grid()
+plt.savefig(os.path.join(path, "td_energy_laser_off.png"))
 
 plt.figure()
 plt.plot(time_points, td_energies_imag)
 plt.title("Time-dependent energy (imaginary component)")
 plt.grid()
+plt.savefig(os.path.join(path, "td_energy_imag.png"))
 
 plt.figure()
-plt.plot(time_points[time_points > 5], td_energies_imag[time_points > 5])
+plt.plot(
+    time_points[time_points > laser_duration],
+    td_energies_imag[time_points > laser_duration],
+)
 plt.title(
     "Time-dependent energy, after laser is turned off (imaginary component)"
 )
 plt.grid()
+plt.savefig(os.path.join(path, "td_energy_imag_laser_off.png"))
 
 plt.figure()
 plt.plot(time_points, dip_z)
 plt.title(r"$\langle z(t) \rangle$")
 plt.grid()
+plt.savefig(os.path.join(path, "exp_z.png"))
 
 plt.figure()
 plt.plot(time_points, norm_t2)
 plt.title(r"Norm of $\tau_2$-amplitudes")
 plt.grid()
+plt.savefig(os.path.join(path, "norm_t_2.png"))
 
 plt.figure()
 plt.plot(time_points, norm_l2)
 plt.title(r"Norm of $\lambda_2$-amplitudes")
 plt.grid()
-plt.show()
+plt.savefig(os.path.join(path, "norm_l_2.png"))
 
 from scipy.fftpack import fft, ifft, fftshift, fftfreq
+
 """
 Fourier transform of dip_z after pulse.
 """
-freq = fftshift(fftfreq(len(time_points[timestep_stop_laser:]))) * (2 * np.pi / dt)
-a = np.abs(fftshift(fft(dip_z[timestep_stop_laser:])))
+freq = fftshift(fftfreq(len(time_points[time_points > laser_duration]))) * (
+    2 * np.pi / dt
+)
+a = np.abs(fftshift(fft(dip_z[time_points > laser_duration])))
 amax = a.max()
 a = a / amax
 plt.figure()
@@ -169,4 +195,5 @@ plt.title(r"Fourier transform of $\langle z(t)\rangle$")
 plt.legend()
 plt.xlim(0, 6)
 plt.xlabel("frequency/au")
+plt.savefig(os.path.join(path, "dip_z_laser_oss.png"))
 plt.show()
