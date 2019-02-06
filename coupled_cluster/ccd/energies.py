f9bb5ff3d66d4acc90181b2e0534f8cf7e45f91a
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-06 10:06:00 +0100

    Rewrite lagrangian in terms of tensordot
            This takes 2/3 of the time spent in the einsum-version.

--- coupled_cluster/ccd/energies.py
+++ coupled_cluster/ccd/energies.py
@@ -24,101 +24,296 @@ def compute_time_dependent_energy(f, u, t, l, o, v, np):
 
 
 def compute_oatdccd_energy(f, u, t, l, o, v, np):
+    # import time
+
     energy = compute_reference_energy(f, u, o, v, np=np)
-    energy += lagrangian_funtional(
-        t.transpose(2, 3, 0, 1), l, f, u, o, v, np=np
-    )
+    # energy += lagrangian_funtional(
+    #    t.transpose(2, 3, 0, 1), l, f, u, o, v, np=np
+    # )
+    energy += lagrangian_funtional(f, u, t, l, o, v, np=np)
+    # t0 = time.time()
+    # lagrangian_trans = lagrangian_funtional_trans(
+    #    t.transpose(2, 3, 0, 1), l, f, u, o, v, np=np
+    # )
+    # t1 = time.time()
+    # total = t1 - t0
+    # print(f"Trans: {total} sec")
+    # t0 = time.time()
+    # lagrangian = lagrangian_funtional(f, u, t, l, o, v, np=np)
+    # t1 = time.time()
+    # total = t1 - t0
+    # print(f"Non  : {total} sec")
+
+    # assert abs(lagrangian_trans - lagrangian) < 1e-12
+
+    # energy += lagrangian
 
     return energy
 
 
-def lagrangian_funtional(T2, L2, F, W, o, v, np):
+def lagrangian_funtional(f, u, t, l, o, v, np):
     """
     Eq [A8] in Kvaal sans the reference energy
 
         E_ref = h^i_i + 0.5 u^{ij}_{ij}
             = f^{i}_{i} - 0.5 u^{ij}_{ij}
     """
-    result = 0.25 * np.einsum(
-        "lkdc,dclk->", L2, W[v, v, o, o], optimize=["einsum_path", (0, 1)]
-    )
-    result += 0.25 * np.einsum(
-        "lkdc,lkdc->", T2, W[o, o, v, v], optimize=["einsum_path", (0, 1)]
-    )
-    result += np.einsum(
-        "lkdc,kmec,dmel->",
-        L2,
-        T2,
-        W[v, o, v, o],
-        optimize=["einsum_path", (0, 1), (0, 1)],
-    )
-    result += 0.5 * np.einsum(
-        "lkdc,kmdc,ml->",
-        L2,
-        T2,
-        F[o, o],
-        optimize=["einsum_path", (0, 1), (0, 1)],
-    )
-    result += 0.5 * np.einsum(
-        "lkdc,lkec,de->",
-        L2,
-        T2,
-        F[v, v],
-        optimize=["einsum_path", (0, 1), (0, 1)],
-    )
-    result += 0.125 * np.einsum(
-        "lkdc,mndc,mnlk->",
-        L2,
-        T2,
-        W[o, o, o, o],
-        optimize=["einsum_path", (0, 1), (0, 1)],
-    )
-    result += 0.125 * np.einsum(
-        "lkdc,lkef,dcef->",
-        L2,
-        T2,
-        W[v, v, v, v],
-        optimize=["einsum_path", (0, 2), (0, 1)],
-    )
-    result += 0.5 * np.einsum(
-        "lkdc,kndf,lmec,mnef->",
-        L2,
-        T2,
-        T2,
-        W[o, o, v, v],
-        optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
-    )
-    result += -0.25 * np.einsum(
-        "lkdc,lkdf,mnec,mnef->",
-        L2,
-        T2,
-        T2,
-        W[o, o, v, v],
-        optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
-    )
-    result += -0.125 * np.einsum(
-        "lkdc,kndc,lmef,mnef->",
-        L2,
-        T2,
-        T2,
-        W[o, o, v, v],
-        optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
-    )
-    result += -0.125 * np.einsum(
-        "lkdc,lmdc,knef,mnef->",
-        L2,
-        T2,
-        T2,
-        W[o, o, v, v],
-        optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
-    )
-    result += 0.0625 * np.einsum(
-        "lkdc,mndc,lkef,mnef->",
-        L2,
-        T2,
-        T2,
-        W[o, o, v, v],
-        optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
-    )
+    # result = 0.25 * np.einsum(
+    #    "lkdc,dclk->", l, u[v, v, o, o], optimize=["einsum_path", (0, 1)]
+    # )
 
-    return result
+    energy = 0.25 * np.tensordot(
+        l, u[v, v, o, o], axes=((0, 1, 2, 3), (2, 3, 0, 1))
+    )
+    # np.testing.assert_allclose(result, energy)
+
+    # result += 0.25 * np.einsum(
+    #    "dclk,lkdc->", t, u[o, o, v, v], optimize=["einsum_path", (0, 1)]
+    # )
+
+    energy += 0.25 * np.tensordot(
+        t, u[o, o, v, v], axes=((0, 1, 2, 3), (2, 3, 0, 1))
+    )
+    # np.testing.assert_allclose(result, energy)
+
+    # result += np.einsum(
+    #    "lkdc,eckm,dmel->",
+    #    l,
+    #    t,
+    #    u[v, o, v, o],
+    #    optimize=["einsum_path", (0, 1), (0, 1)],
+    # )
+
+    temp_ldem = np.tensordot(l, t, axes=((1, 3), (2, 1)))
+    energy += np.tensordot(
+        temp_ldem, u[v, o, v, o], axes=((0, 1, 2, 3), (3, 0, 2, 1))
+    )
+    # np.testing.assert_allclose(result, energy)
+
+    # result += 0.5 * np.einsum(
+    #    "lkdc,dckm,ml->",
+    #    l,
+    #    t,
+    #    f[o, o],
+    #    optimize=["einsum_path", (0, 1), (0, 1)],
+    # )
+
+    temp_dckl = 0.5 * np.tensordot(t, f[o, o], axes=((3), (0)))
+    energy += np.tensordot(l, temp_dckl, axes=((0, 1, 2, 3), (3, 2, 0, 1)))
+    # np.testing.assert_allclose(result, energy)
+
+    # result += 0.5 * np.einsum(
+    #    "lkdc,eclk,de->",
+    #    l,
+    #    t,
+    #    f[v, v],
+    #    optimize=["einsum_path", (0, 1), (0, 1)],
+    # )
+
+    temp_dclk = 0.5 * np.tensordot(f[v, v], t, axes=((1), (0)))
+    energy += np.tensordot(temp_dclk, l, axes=((0, 1, 2, 3), (2, 3, 0, 1)))
+    # np.testing.assert_allclose(result, energy)
+
+    # result += 0.125 * np.einsum(
+    #    "lkdc,dcmn,mnlk->",
+    #    l,
+    #    t,
+    #    u[o, o, o, o],
+    #    optimize=["einsum_path", (0, 1), (0, 1)],
+    # )
+
+    temp_dclk = 0.125 * np.tensordot(t, u[o, o, o, o], axes=((2, 3), (0, 1)))
+    energy += np.tensordot(temp_dclk, l, axes=((0, 1, 2, 3), (2, 3, 0, 1)))
+    # np.testing.assert_allclose(result, energy)
+
+    # result += 0.125 * np.einsum(
+    #    "lkdc,eflk,dcef->",
+    #    l,
+    #    t,
+    #    u[v, v, v, v],
+    #    optimize=["einsum_path", (0, 2), (0, 1)],
+    # )
+
+    temp_dclk = 0.125 * np.tensordot(u[v, v, v, v], t, axes=((2, 3), (0, 1)))
+    energy += np.tensordot(temp_dclk, l, axes=((0, 1, 2, 3), (2, 3, 0, 1)))
+    # np.testing.assert_allclose(result, energy)
+
+    # result += 0.5 * np.einsum(
+    #    "lkdc,dfkn,eclm,mnef->",
+    #    l,
+    #    t,
+    #    t,
+    #    u[o, o, v, v],
+    #    optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
+    # )
+
+    temp_clnf = 0.5 * np.tensordot(t, u[o, o, v, v], axes=((0, 3), (2, 0)))
+    temp_kdnf = np.tensordot(l, temp_clnf, axes=((0, 3), (1, 0)))
+    energy += np.tensordot(t, temp_kdnf, axes=((0, 1, 2, 3), (1, 3, 0, 2)))
+    # np.testing.assert_allclose(result, energy)
+
+    # result += -0.25 * np.einsum(
+    #    "lkdc,dflk,ecmn,mnef->",
+    #    l,
+    #    t,
+    #    t,
+    #    u[o, o, v, v],
+    #    optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
+    # )
+
+    temp_cf = -0.25 * np.tensordot(
+        t, u[o, o, v, v], axes=((0, 2, 3), (2, 0, 1))
+    )
+    temp_lkdf = np.tensordot(l, temp_cf, axes=((3), (0)))
+    energy += np.tensordot(temp_lkdf, t, axes=((0, 1, 2, 3), (2, 3, 0, 1)))
+    # np.testing.assert_allclose(result, energy)
+
+    # result += -0.125 * np.einsum(
+    #    "lkdc,dckn,eflm,mnef->",
+    #    l,
+    #    t,
+    #    t,
+    #    u[o, o, v, v],
+    #    optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
+    # )
+
+    temp_ln = -0.125 * np.tensordot(
+        t, u[o, o, v, v], axes=((0, 1, 3), (2, 3, 0))
+    )
+    temp_dckl = np.tensordot(t, temp_ln, axes=((3), (1)))
+    energy += np.tensordot(l, temp_dckl, axes=((0, 1, 2, 3), (3, 2, 0, 1)))
+    # np.testing.assert_allclose(result, energy)
+
+    # result += -0.125 * np.einsum(
+    #    "lkdc,dclm,efkn,mnef->",
+    #    l,
+    #    t,
+    #    t,
+    #    u[o, o, v, v],
+    #    optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
+    # )
+
+    temp_km = -0.125 * np.tensordot(l, t, axes=((0, 2, 3), (2, 0, 1)))
+    temp_knef = np.tensordot(temp_km, u[o, o, v, v], axes=((1), (0)))
+    energy += np.tensordot(temp_knef, t, axes=((0, 1, 2, 3), (2, 3, 0, 1)))
+    # np.testing.assert_allclose(result, energy)
+
+    # result += 0.0625 * np.einsum(
+    #    "lkdc,dcmn,eflk,mnef->",
+    #    l,
+    #    t,
+    #    t,
+    #    u[o, o, v, v],
+    #    optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
+    # )
+
+    if o.stop >= v.stop // 2:
+        temp_dcef = 0.0625 * np.tensordot(
+            t, u[o, o, v, v], axes=((2, 3), (0, 1))
+        )
+        temp_efdc = np.tensordot(t, l, axes=((2, 3), (0, 1)))
+        energy += np.tensordot(
+            temp_efdc, temp_dcef, axes=((0, 1, 2, 3), (2, 3, 0, 1))
+        )
+    else:
+        temp_lkmn = 0.0625 * np.tensordot(l, t, axes=((2, 3), (0, 1)))
+        temp_mnlk = np.tensordot(u[o, o, v, v], t, axes=((2, 3), (0, 1)))
+        energy += np.tensordot(
+            temp_lkmn, temp_mnlk, axes=((0, 1, 2, 3), (2, 3, 0, 1))
+        )
+    # np.testing.assert_allclose(result, energy)
+
+    # return result
+    return energy
+
+
+# def lagrangian_funtional_trans(T2, L2, F, W, o, v, np):
+#    """
+#   Eq [A8] in Kvaal sans the reference energy
+#
+#       E_ref = h^i_i + 0.5 u^{ij}_{ij}
+#           = f^{i}_{i} - 0.5 u^{ij}_{ij}
+#   """
+#    result = 0.25 * np.einsum(
+#        "lkdc,dclk->", L2, W[v, v, o, o], optimize=["einsum_path", (0, 1)]
+#    )
+#    result += 0.25 * np.einsum(
+#        "lkdc,lkdc->", T2, W[o, o, v, v], optimize=["einsum_path", (0, 1)]
+#    )
+#    result += np.einsum(
+#        "lkdc,kmec,dmel->",
+#        L2,
+#        T2,
+#        W[v, o, v, o],
+#        optimize=["einsum_path", (0, 1), (0, 1)],
+#    )
+#    result += 0.5 * np.einsum(
+#        "lkdc,kmdc,ml->",
+#        L2,
+#        T2,
+#        F[o, o],
+#        optimize=["einsum_path", (0, 1), (0, 1)],
+#    )
+#    result += 0.5 * np.einsum(
+#        "lkdc,lkec,de->",
+#        L2,
+#        T2,
+#        F[v, v],
+#        optimize=["einsum_path", (0, 1), (0, 1)],
+#    )
+#    result += 0.125 * np.einsum(
+#        "lkdc,mndc,mnlk->",
+#        L2,
+#        T2,
+#        W[o, o, o, o],
+#        optimize=["einsum_path", (0, 1), (0, 1)],
+#    )
+#    result += 0.125 * np.einsum(
+#        "lkdc,lkef,dcef->",
+#        L2,
+#        T2,
+#        W[v, v, v, v],
+#        optimize=["einsum_path", (0, 2), (0, 1)],
+#    )
+#    result += 0.5 * np.einsum(
+#        "lkdc,kndf,lmec,mnef->",
+#        L2,
+#        T2,
+#        T2,
+#        W[o, o, v, v],
+#        optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
+#    )
+#    result += -0.25 * np.einsum(
+#        "lkdc,lkdf,mnec,mnef->",
+#        L2,
+#        T2,
+#        T2,
+#        W[o, o, v, v],
+#        optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
+#    )
+#    result += -0.125 * np.einsum(
+#        "lkdc,kndc,lmef,mnef->",
+#        L2,
+#        T2,
+#        T2,
+#        W[o, o, v, v],
+#        optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
+#    )
+#    result += -0.125 * np.einsum(
+#        "lkdc,lmdc,knef,mnef->",
+#        L2,
+#        T2,
+#        T2,
+#        W[o, o, v, v],
+#        optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
+#    )
+#    result += 0.0625 * np.einsum(
+#        "lkdc,mndc,lkef,mnef->",
+#        L2,
+#        T2,
+#        T2,
+#        W[o, o, v, v],
+#        optimize=["einsum_path", (0, 1), (0, 1), (0, 1)],
+#    )
+#
+#    return result
