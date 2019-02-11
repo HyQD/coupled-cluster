d1ca8ba38990b878c552d6bdf9cbde8d43525d24
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-11 17:20:54 +0100

    Add function generating CC lagrangian

---
+++ symbolic-expressions/generate_cc_lagrangian.py
@@ -0,0 +1,35 @@
+from sympy.physics.secondquant import Commutator, wicks
+from sympy import factorial
+from cluster_operators import get_hamiltonian, get_clusters
+from helper_functions import eval_equation
+
+
+def generate_lagrangian(t_operators, l_operators):
+    hamiltonian = get_hamiltonian()
+
+    comm_term = sum(hamiltonian)
+    equation = wicks(comm_term)
+    i = 0
+
+    while comm_term != 0:
+        t = get_clusters(t_operators)
+
+        comm_term = wicks(Commutator(comm_term, t))
+        equation += comm_term / factorial(i + 1)
+        i += 1
+
+    equation = (1 + get_clusters(l_operators)) * equation
+
+    return eval_equation(equation)
+
+
+if __name__ == "__main__":
+    from cluster_operators import get_t_2_operator, get_l_2_operator
+    from sympy import latex
+
+    lagrangian = generate_lagrangian(get_t_2_operator, get_l_2_operator)
+
+    print(latex(lagrangian))
+    print("\n\n")
+    for term in lagrangian.args:
+        print(latex(term))
