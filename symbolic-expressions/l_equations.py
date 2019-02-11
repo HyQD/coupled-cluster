78544fa64adbdffb3796a3363baebf5dfa429105
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-11 20:10:26 +0100

    Make dog-slow lambda equation generator

--- symbolic-expressions/l_equations.py
+++ symbolic-expressions/l_equations.py
@@ -5,24 +5,32 @@ from sympy.physics.secondquant import (
     NO,
     Fd,
     F,
+    wicks,
 )
 from hamiltonian import get_hamiltonian
 from sympy import factorial, Rational, symbols
-from cluster_operators import get_clusters, get_t_2_operator, get_l_2_operator
+from cluster_operators import (
+    get_clusters,
+    get_t_2_operator,
+    get_l_2_operator,
+    get_hamiltonian,
+)
 from helper_functions import beautify_equation, eval_equation
 
 
-def compute_hausdorff(comm_term, t_func, num_terms=4):
-    equation = comm_term
+def compute_hausdorff(comm_term, t_operators, l_operators):
+    equation = wicks(comm_term)
+    i = 0
 
-    for i in range(num_terms):
-        T = get_clusters(t_func)
+    while comm_term != 0:
+        t = get_clusters(t_operators)
 
-        comm_term = eval_equation(Commutator(comm_term, T), wicks_kwargs={})
-        # comm_term = wicks(Commutator(comm_term, T))
-        # comm_term = substitute_dummies(evaluate_deltas(comm_term))
+        comm_term = wicks(Commutator(comm_term, t))
+        equation += comm_term / factorial(i + 1)
+        i += 1
+        print(i)
 
-        equation += Rational(1, factorial(i + 1)) * comm_term
+    equation = (1 + get_clusters(l_operators)) * equation
 
     return eval_equation(equation)
 
@@ -37,6 +45,15 @@ def get_similarity_transformed_operator(t_func, num_terms=4):
 
 def get_lagrange_multipliers(l_func):
     return 1 + get_clusters(l_func)
+
+
+def get_doubles_operator():
+    i, j = symbols("i, j", below_fermi=True)
+    a, b = symbols("a, b", above_fermi=True)
+
+    X_ijab = NO(Fd(i) * Fd(j) * F(b) * F(a))
+
+    return X_ijab
 
 
 def get_doubles_amplitudes(eq):
@@ -55,13 +72,12 @@ def get_doubles_amplitudes(eq):
 def get_ccd_l_equations():
     t_func = get_t_2_operator
     l_func = get_l_2_operator
+    H = sum(get_hamiltonian())
+    X_ijab = get_doubles_operator()
 
-    eq = get_similarity_transformed_operator(t_func, num_terms=2)
-    # eq = get_lagrange_multipliers(l_func) * eq
+    comm_term = Commutator(H, X_ijab)
 
-    eq = get_doubles_amplitudes(eq)
-
-    return eval_equation(eq)
+    return eval_equation(compute_hausdorff(comm_term, t_func, l_func))
 
 
 if __name__ == "__main__":
