Fix sign in d8a-diagram and add note
    There is probably an error in Shavitt & Bartlett.

coupled_cluster/ccsd/ccsd.py
@@ -404,7 +404,7 @@ class CoupledClusterSinglesDoubles(CoupledCluster):
         ccsd_t.add_d5e_t(self.u, self.t_1, self.t_2, o, v, out, np)
         ccsd_t.add_d7b_t(0.5 * self.u, self.t_1, self.t_2, o, v, out, np)
         ccsd_t.add_d6a_t(self.u, self.t_1, o, v, out, np)
-        ccsd_t.add_d8a_t(-self.u, self.t_1, o, v, out, np)
+        ccsd_t.add_d8a_t(self.u, self.t_1, o, v, out, np)
         ccsd_t.add_d7a_t(0.5 * self.u, self.t_1, self.t_2, o, v, out, np)
         ccsd_t.add_d9_t(0.5 * self.u, self.t_1, o, v, out, np)
 
@@ -457,10 +457,10 @@ class CoupledClusterSinglesDoubles(CoupledCluster):
         ###
 
         # TODO: Remove this test
-        out = ccsd_t.compute_t_2_amplitudes(
+        out_2 = ccsd_t.compute_t_2_amplitudes(
             f, self.u, self.t_1, self.t_2, o, v, np=np
         )
-        np.testing.assert_allclose(self.rhs_t_2, out)
+        np.testing.assert_allclose(self.rhs_t_2, out_2, atol=1e-7)
 
     def _compute_ccsd_lambda_amplitudes_s(self):
         np = self.np

coupled_cluster/ccsd/rhs_t.py
@@ -508,9 +508,13 @@ def add_d7e_t(u, t_1, t_2, o, v, out, np):
 def add_d8a_t(u, t_1, o, v, out, np):
     """Function for adding the D8a diagram
 
-        g(f, u, t) <- u^{kb}_{cd} t^{c}_{i} t^{a}_{k} t^{d}_{j} P(ab)
+        g(f, u, t) <- (-1) * u^{kb}_{cd} t^{c}_{i} t^{a}_{k} t^{d}_{j} P(ab)
     
     Number of FLOPS required O()
+
+    Note: The minus sign in this expression is in disagreement with the
+    definition in Shavitt & Bartlett page 306. We believe this is an error in
+    Shavitt & Bartlett as SymPy and Gauss & Stanton's article are in agreement.
     """
 
     term = np.tensordot(u[o, v, v, v], t_1, axes=((2), (0)))  # kbdi
@@ -520,7 +524,7 @@ def add_d8a_t(u, t_1, o, v, out, np):
     )  # biaj -> abij
     term -= term.swapaxes(0, 1)
 
-    out += term
+    out -= term
 
 
 def add_d8b_t(u, t_1, o, v, out, np):