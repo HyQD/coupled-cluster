9ee10fb8142047885367e4a8311b092bcbfb5319
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-03-02 21:15:52 +0100

    Add comparison using HF-basis

--- scripts/run_one_dim_ho_tdccsd.py
+++ scripts/run_one_dim_ho_tdccsd.py
@@ -2,6 +2,7 @@ from quantum_systems import OneDimensionalHarmonicOscillator
 from quantum_systems.time_evolution_operators import LaserField
 from coupled_cluster.ccsd import CoupledClusterSinglesDoubles
 from coupled_cluster.ccd import CoupledClusterDoubles
+from tdhf import HartreeFock
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -35,6 +36,10 @@ laser = LaserField(
 )
 odho_ccsd.set_time_evolution_operator(laser)
 
+hf_ccsd = HartreeFock(odho_ccsd, verbose=True)
+C = hf_ccsd.scf(tolerance=1e-6)
+odho_ccsd.change_basis(C)
+
 odho_ccd = OneDimensionalHarmonicOscillator(
     n, l, length, num_grid_points, omega=omega
 )
@@ -44,19 +49,25 @@ laser = LaserField(
 )
 odho_ccd.set_time_evolution_operator(laser)
 
+hf_ccd = HartreeFock(odho_ccd, verbose=True)
+C = hf_ccd.scf(tolerance=1e-6)
+odho_ccd.change_basis(C)
+
 ccsd = CoupledClusterSinglesDoubles(
     odho_ccsd, verbose=True, include_singles=True
 )
 ccd = CoupledClusterDoubles(odho_ccd, verbose=True)
 
-ccsd.compute_ground_state_energy(theta=theta, tol=tol)
-ccd.compute_ground_state_energy(theta=theta, tol=tol)
+ccsd.iterate_t_amplitudes(theta=theta, tol=tol)
+print("Ground state energy (CCSD): {0}".format(ccsd.compute_energy()))
+ccd.iterate_t_amplitudes(theta=theta, tol=tol)
+print("Ground state energy (CCD): {0}".format(ccd.compute_energy()))
 
-ccsd.compute_l_amplitudes(theta=theta, tol=tol, max_iterations=100)
-ccd.compute_l_amplitudes(theta=theta, tol=tol, max_iterations=100)
+ccsd.iterate_l_amplitudes(theta=theta, tol=tol, max_iterations=100)
+ccd.iterate_l_amplitudes(theta=theta, tol=tol, max_iterations=100)
 
-rho_ccsd = ccsd.compute_spin_reduced_one_body_density_matrix()
-rho_ccd = ccd.compute_spin_reduced_one_body_density_matrix()
+rho_ccsd = ccsd.compute_particle_density()
+rho_ccd = ccd.compute_particle_density()
 
 plt.plot(odho_ccsd.grid, rho_ccsd, label=r"$\rho_{CCSD}$")
 plt.plot(odho_ccd.grid, rho_ccd, label=r"$\rho_{CCD}$")
