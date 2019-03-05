aa8b387c3e619386edbc9c72180996aaf0c45111
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-03-05 16:48:38 +0100

    Move transformation of u to helper

--- coupled_cluster/cc_helper.py
+++ coupled_cluster/cc_helper.py
@@ -244,6 +244,19 @@ def remove_diagonal_in_matrix(matrix, np):
     return off_diag
 
 
+def transform_two_body_tensor(u, C, C_tilde, np):
+    # abcd, ds -> abcs
+    _u = np.tensordot(u, C, axes=(3, 0))
+    # abcs, cr -> absr -> abrs
+    _u = np.tensordot(_u, C, axes=(2, 0)).transpose(0, 1, 3, 2)
+    # abrs, qb -> arsq -> aqrs
+    _u = np.tensordot(_u, C_tilde, axes=(1, 1)).transpose(0, 3, 1, 2)
+    # pa, aqrs -> pqrs
+    _u = np.tensordot(C_tilde, _u, axes=(1, 0))
+
+    return _u
+
+
 def construct_d_t_1_matrix(f, o, v, np):
     f_diag = np.diag(f)
     d_t_1 = f_diag[o] - f_diag[v].reshape(-1, 1)
