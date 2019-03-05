5ae5ab82499dd1597c7e29fb981ebf57cbe960ae
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-03-05 15:54:32 +0100

    Move construction of d-matrix to helper functions

--- coupled_cluster/cc_helper.py
+++ coupled_cluster/cc_helper.py
@@ -242,3 +242,22 @@ def remove_diagonal_in_matrix(matrix, np):
     np.fill_diagonal(off_diag, 0)
 
     return off_diag
+
+
+def construct_d_t_1_matrix(f, o, v, np):
+    f_diag = np.diag(f)
+    d_t_1 = f_diag[o] - f_diag[v].reshape(-1, 1)
+
+    return d_t_1
+
+
+def construct_d_t_2_matrix(f, o, v, np):
+    f_diag = np.diag(f)
+    d_t_2 = (
+        f_diag[o]
+        + f_diag[o].reshape(-1, 1)
+        - f_diag[v].reshape(-1, 1, 1)
+        - f_diag[v].reshape(-1, 1, 1, 1)
+    )
+
+    return d_t_2
