Add actual computation of lambda amplitudes. Fix tests.

coupled_cluster/ccsd/ccsd.py
@@ -28,10 +28,10 @@ class CoupledClusterSinglesDoubles(CoupledCluster):
         n, m = self.n, self.m
 
         self.include_singles = include_singles
-        
+
         # Singles
-        self.rhs_t_1 = np.zeros((m, n), dtype=self.f.dtype)  # ai
-        self.rhs_l_1 = np.zeros((n, m), dtype=self.f.dtype)  # ia
+        self.rhs_t_1 = np.zeros((m, n), dtype=self.u.dtype)  # ai
+        self.rhs_l_1 = np.zeros((n, m), dtype=self.u.dtype)  # ia
 
         self.t_1 = np.zeros_like(self.rhs_t_1)
         self.l_1 = np.zeros_like(self.rhs_l_1)

tests/test_compare_methods.py
@@ -41,14 +41,12 @@ def test_ground_state(ccd, ccsd_sans_singles, t_kwargs):
     ccsd.iterate_l_amplitudes(**t_kwargs)
 
     np.testing.assert_allclose(ccsd.l_1, np.zeros_like(ccsd.l_1), atol=1e-10)
-    # FAILING
-    # np.testing.assert_allclose(ccsd.l_2, ccd.l_2, atol=1e-10)
+    np.testing.assert_allclose(ccsd.l_2, ccd.l_2, atol=1e-10)
 
     rho_ccd = ccd.compute_particle_density()
     rho_ccsd = ccsd.compute_particle_density()
 
-    # FAILING
-    # np.testing.assert_allclose(rho_ccsd, rho_ccd, atol=1e-10)
+    np.testing.assert_allclose(rho_ccsd, rho_ccd, atol=1e-10)
 
 
 @pytest.mark.skip