248f3f152d4640053158a21d1ac0a45c9604c7f3
gregwinther <sebastwi@uio.no>
2019-03-15 17:30:32 +0100

    Begin TDCCSD Helium analysis.

---
+++ scripts/tdccsd_he.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib
+matplotlib.use("TKAgg")
+import matplotlib.pyplot as plt
+import tqdm
+
+from quantum_systems import construct_psi4_system
+from quantum_systems.time_evolution_operators import LaserField
+from tdhf import HartreeFock
+from coupled_cluster.ccsd import TDCCSD
+from coupled_cluster.integrators import GaussIntegrator
+
+
+class LaserPulse:
+    def __init__(self, t0=0, td=5, omega=0.1, E=0.03):
+        self.t0 = t0
+        self.td = td
+        self.omega = omega
+        self.E = E  # Field strength
+
+    def __call__(self, t):
+        T = self.td
+        delta_t = t - self.t0
+        return (
+            -(np.sin(np.pi * delta_t / T) ** 2)
+            * np.heaviside(delta_t, 1.0)
+            * np.heaviside(T - delta_t, 1.0)
+            * np.cos(self.omega * delta_t)
+            * self.E
+        )
+
+
+# System parameters
+He = """
+    He 0.0 0.0 0.0
+    symmetry c1
+    """
+
+options = {"basis": "cc-pvdz", "scf_type": "pk", "e_convergence": 1e-6}
+omega = 2.873_564_3
+E = 1
+laser_duration = 5
+
+system = construct_psi4_system(He, options)
+hf = HartreeFock(system, verbose=True)
+C = hf.scf(tolerance=1e-15)
+system.change_basis(C)
+
+integrator = GaussIntegrator(np=np, eps=1e-10)
+tdccsd = TDCCSD(system, integrator=integrator, np=np, verbose=True)
+t_kwargs = dict(theta=0, tol=1e-10)
+tdccsd.compute_ground_state(t_kwargs=t_kwargs, l_kwargs=t_kwargs)
+print(
+    f"Ground state CCSD energy: {tdccsd.compute_ground_state_energy()}"
+)
+
+polarization = np.zeros(3)
+polarization[2] = 1
+system.set_time_evolution_operator(
+    LaserField(
+        LaserPulse(td=laser_duration, omega=omega, E=E),
+        polarization_vector=polarization
+    )
+)
+
+tdccsd.set_initial_conditions()
+dt = 1e-2
+T = 10
+num_steps = int(T // dt) + 1
+t_stop_laser = int(laser_duration // dt)
+
+time_points = np.linspace(0, T, num_steps)
+print(f"Total iterations: {num_steps}")
+
+td_energies = np.zeros(len(time_points))
+# td_energies_imag = np.zeros(len(time_points))
+td_energies[0] = tdccsd.compute_energy()
+
+for i, amp in enumerate(tqdm.tqdm(tdccsd.solve(time_points))):
+    # t_1, t_2, l_1, l_2 = amp
+    energy = tdccsd.compute_energy()
+    td_energies[i + 1] = energy.real
+    # td_energies_imag[i + 1] = energy.imag
+
+plt.figure()
+plt.plot(time_points[time_points > laser_duration], td_energies[time_points > laser_duration])
+plt.title("Energy after laser is turned off")
+plt.grid()
+
+plt.show()
