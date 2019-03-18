a62a247747b8a33cdfdd120c79fdd225f0635f13
Travis CI <travis@travis-ci.org>
2019-03-18 12:28:24 +0000

    Black files

--- coupled_cluster/ccsd/tdccsd.py
+++ coupled_cluster/ccsd/tdccsd.py
@@ -13,7 +13,7 @@ from coupled_cluster.ccsd.density_matrices import (
     compute_one_body_density_matrix,
 )
 from coupled_cluster.ccsd.time_dependent_overlap import (
-    compute_time_dependent_overlap
+    compute_time_dependent_overlap,
 )
 
 
@@ -33,7 +33,15 @@ class TDCCSD(TimeDependentCoupledCluster):
         t_1, t_2, l_1, l_2 = self._amplitudes.unpack()
 
         return compute_time_dependent_energy(
-            self.f, self.u, t_1, t_2, l_1, l_2, self.system.o, self.system.v, np=self.np
+            self.f,
+            self.u,
+            t_1,
+            t_2,
+            l_1,
+            l_2,
+            self.system.o,
+            self.system.v,
+            np=self.np,
         )
 
     def compute_one_body_density_matrix(self):
@@ -50,12 +58,19 @@ class TDCCSD(TimeDependentCoupledCluster):
         t_1, t_2, l_1, l_2 = self._amplitudes.unpack()
 
         # print(t_1)
-        assert not self.np.any(self.np.isnan(t_1))         
-        assert not self.np.any(self.np.isnan(t_2)) 
-        assert not self.np.any(self.np.isnan(l_1)) 
-        assert not self.np.any(self.np.isnan(l_2)) 
+        assert not self.np.any(self.np.isnan(t_1))
+        assert not self.np.any(self.np.isnan(t_2))
+        assert not self.np.any(self.np.isnan(l_1))
+        assert not self.np.any(self.np.isnan(l_2))
 
         return compute_time_dependent_overlap(
-            self.cc.t_1, self.cc.t_2, self.cc.l_1, self.cc.l_2,
-            t_1, t_2, l_1, l_2, np=self.np
+            self.cc.t_1,
+            self.cc.t_2,
+            self.cc.l_1,
+            self.cc.l_2,
+            t_1,
+            t_2,
+            l_1,
+            l_2,
+            np=self.np,
         )
