0d1c2cfc7b383fdf7ddd7494d21e5486be5047f9
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-03-20 12:26:50 +0100

    Use ssh access to git with deploy keys

--- push_formatting.sh
+++ push_formatting.sh
@@ -6,5 +6,6 @@ user_email="$GH_USER_EMAIL"
 if [ -n "$(git status --porcelain)" ]; then
     git add .
     git -c user.name="$user_name" -c user.email="$user_email" commit -m "Black files"
-    git push https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:$TRAVIS_BRANCH --quiet
+    git push git@github.com:$TRAVIS_REPO_SLUG HEAD:$TRAVIS_BRANCH --quiet
+    #git push https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:$TRAVIS_BRANCH --quiet
 fi
