97b035bf3d5bd68a71ae8df232687efc279cc9d3
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-03-27 17:23:46 +0100

    Return array for tau-0

--- coupled_cluster/cc.py
+++ coupled_cluster/cc.py
@@ -38,7 +38,9 @@ class CoupledCluster(metaclass=abc.ABCMeta):
     def get_amplitudes(self, get_t_0=False):
         if get_t_0:
             return AmplitudeContainer(
-                t=[0, *self._get_t_copy()], l=self._get_l_copy(), np=self.np
+                t=[self.np.array([0]), *self._get_t_copy()],
+                l=self._get_l_copy(),
+                np=self.np,
             )
 
         return AmplitudeContainer(
