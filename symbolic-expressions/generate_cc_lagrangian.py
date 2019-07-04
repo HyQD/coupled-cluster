6daebcf50bdd90ca84a362e5024e56eb06acd086
gregwinther <sebastwi@uio.no>
2019-07-04 12:15:42 +0200

    Fix CCD langrangian and amplitude generation.

--- symbolic-expressions/generate_cc_lagrangian.py
+++ symbolic-expressions/generate_cc_lagrangian.py
@@ -24,9 +24,9 @@ def generate_lagrangian(t_operators, l_operators):
 
 if __name__ == "__main__":
     from cluster_operators import (
-        get_t_1_operator,
+        #get_t_1_operator,
         get_t_2_operator,
-        get_l_1_operator,
+        #get_l_1_operator,
         get_l_2_operator,
     )
     from helper_functions import eval_equation
@@ -34,8 +34,8 @@ if __name__ == "__main__":
 
     lagrangian = eval_equation(
         generate_lagrangian(
-            [get_t_1_operator, get_t_2_operator],
-            [get_l_1_operator, get_l_2_operator],
+            [get_t_2_operator],
+            [get_l_2_operator],
         )
     )
 
