5bbfe31a99b1f6b16a9da786425432fac106fb75
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-08-17 17:39:02 +0200

    Add test of TDCCSD comparing with TDCISD

--- tests/test_tdccsd.py
+++ tests/test_tdccsd.py
@@ -1,5 +1,33 @@
+import sys
+
+sys.path.append(".")
+import os
 import numpy as np
+
+from quantum_systems import construct_pyscf_system
+from quantum_systems.time_evolution_operators import LaserField
 from coupled_cluster.ccsd.energies import lagrangian_functional
+from coupled_cluster.ccsd import TDCCSD
+from coupled_cluster.integrators import GaussIntegrator
+
+
+class LaserPulse:
+    def __init__(self, t0=0, td=5, omega=0.1, E=0.03):
+        self.t0 = t0
+        self.td = td
+        self.omega = omega
+        self.E = E  # Field strength
+
+    def __call__(self, t):
+        T = self.td
+        delta_t = t - self.t0
+        return (
+            -(np.sin(np.pi * delta_t / T) ** 2)
+            * np.heaviside(delta_t, 1.0)
+            * np.heaviside(T - delta_t, 1.0)
+            * np.cos(self.omega * delta_t)
+            * self.E
+        )
 
 
 def test_lagrangian_functional(large_system_ccsd):
@@ -184,3 +212,92 @@ def test_lagrangian_functional(large_system_ccsd):
     )
 
     assert abs(result - energy) < 1e-8
+
+
+def test_tdccsd():
+    omega = 2.873_564_3
+    E = 0.1
+    laser_duration = 5
+
+    system = construct_pyscf_system(molecule="he 0.0 0.0 0.0", basis="cc-pvdz")
+
+    integrator = GaussIntegrator(s=3, np=np, eps=1e-6)
+    tdccsd = TDCCSD(system, integrator=integrator, np=np, verbose=True)
+    tdccsd.compute_ground_state()
+    assert (
+        abs(tdccsd.compute_ground_state_energy() - -2.887_594_831_090_936)
+        < 1e-6
+    )
+
+    polarization = np.zeros(3)
+    polarization[2] = 1
+    system.set_time_evolution_operator(
+        LaserField(
+            LaserPulse(td=laser_duration, omega=omega, E=E),
+            polarization_vector=polarization,
+        )
+    )
+
+    tdccsd.set_initial_conditions()
+    dt = 1e-3
+    T = 1
+    num_steps = int(T // dt) + 1
+    t_stop_laser = int(laser_duration // dt) + 1
+
+    time_points = np.linspace(0, T, num_steps)
+
+    td_energies = np.zeros(len(time_points), dtype=np.complex128)
+    dip_z = np.zeros(len(time_points))
+    td_overlap = np.zeros_like(dip_z)
+
+    rho_qp = tdccsd.compute_one_body_density_matrix()
+    rho_qp_hermitian = 0.5 * (rho_qp.conj().T + rho_qp)
+
+    td_energies[0] = tdccsd.compute_energy()
+    dip_z[0] = np.einsum(
+        "qp,pq->", rho_qp_hermitian, system.dipole_moment[2]
+    ).real
+    td_overlap[0] = tdccsd.compute_time_dependent_overlap()
+
+    import tqdm
+
+    for i, c in tqdm.tqdm(
+        enumerate(tdccsd.solve(time_points)), total=num_steps
+    ):
+        td_energies[i + 1] = tdccsd.compute_energy()
+
+        rho_qp = tdccsd.compute_one_body_density_matrix()
+        rho_qp_hermitian = 0.5 * (rho_qp.conj().T + rho_qp)
+
+        dip_z[i + 1] = np.einsum(
+            "qp,pq->", rho_qp_hermitian, system.dipole_moment[2]
+        ).real
+        td_overlap[i + 1] = tdccsd.compute_time_dependent_overlap()
+
+    np.testing.assert_allclose(
+        td_energies.real,
+        np.loadtxt(
+            os.path.join("tests", "dat", "tdcisd_helium_energies_real_0.1.dat")
+        ),
+        atol=1e-7,
+    )
+
+    np.testing.assert_allclose(
+        td_overlap,
+        np.loadtxt(
+            os.path.join("tests", "dat", "tdcisd_helium_overlap_0.1.dat")
+        ),
+        atol=1e-7,
+    )
+
+    np.testing.assert_allclose(
+        dip_z,
+        np.loadtxt(
+            os.path.join("tests", "dat", "tdcisd_helium_dipole_z_0.1.dat")
+        ),
+        atol=1e-7,
+    )
+
+
+if __name__ == "__main__":
+    test_tdccsd()
