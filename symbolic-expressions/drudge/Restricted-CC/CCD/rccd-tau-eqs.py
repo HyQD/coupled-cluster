03e883d86b080d7514aad536799923417afeef43
haakoek <haakoek@student.matnat.uio.no>
2020-02-09 11:42:18 +0100

    Black files, hopefully fix broken Travis

--- symbolic-expressions/drudge/Restricted-CC/CCD/rccd-tau-eqs.py
+++ symbolic-expressions/drudge/Restricted-CC/CCD/rccd-tau-eqs.py
@@ -6,7 +6,7 @@ from sympy import IndexedBase, Rational, symbols
 from drudge import RestrictedPartHoleDrudge, Stopwatch
 
 # Environment setting up.
-conf = SparkConf().setAppName('rccsd')
+conf = SparkConf().setAppName("rccsd")
 ctx = SparkContext(conf=conf)
 dr = RestrictedPartHoleDrudge(ctx)
 dr.full_simplify = False
@@ -16,10 +16,13 @@ e_ = p.e_
 a, b, c, d = p.V_dumms[:4]
 i, j, k, l = p.O_dumms[:4]
 
-t = IndexedBase('t')
+t = IndexedBase("t")
 cluster = dr.einst(
     # t[a, i] * e_[a, i] +
-    Rational(1, 2) * t[a, b, i, j] * e_[a, i] * e_[b, j]
+    Rational(1, 2)
+    * t[a, b, i, j]
+    * e_[a, i]
+    * e_[b, j]
 )
 
 dr.set_n_body_base(t, 2)
@@ -33,7 +36,7 @@ curr = dr.ham
 h_bar = dr.ham
 for order in range(4):
     curr = (curr | cluster).simplify() * Rational(1, order + 1)
-    stopwatch.tock('Commutator order {}'.format(order + 1), curr)
+    stopwatch.tock("Commutator order {}".format(order + 1), curr)
     h_bar += curr
     continue
 
@@ -41,22 +44,24 @@ for order in range(4):
 # In[ ]:
 h_bar = h_bar.simplify()
 h_bar.repartition(cache=True)
-stopwatch.tock('H-bar assembly', h_bar)
+stopwatch.tock("H-bar assembly", h_bar)
 
-#vev = vacuum expectation value
+# vev = vacuum expectation value
 en_eqn = h_bar.eval_fermi_vev().simplify()
-stopwatch.tock('Energy equation', en_eqn)
+stopwatch.tock("Energy equation", en_eqn)
 dr.wick_parallel = 1
 
-proj_doubles = Rational(1,3)*e_[i, a] * e_[j, b]+Rational(1,6)*e_[j, a] * e_[i, b]
+proj_doubles = (
+    Rational(1, 3) * e_[i, a] * e_[j, b] + Rational(1, 6) * e_[j, a] * e_[i, b]
+)
 
 # ### Find equations and print to file
 t2_eqn = (proj_doubles * h_bar).eval_fermi_vev().simplify()
-stopwatch.tock('T2 equation', t2_eqn)
+stopwatch.tock("T2 equation", t2_eqn)
 
-with dr.report('rCCD.html', 'restricted CCD theory') as rep:
-    rep.add('Energy equation', en_eqn)
-    rep.add('T2-amplitude equation', t2_eqn)
+with dr.report("rCCD.html", "restricted CCD theory") as rep:
+    rep.add("Energy equation", en_eqn)
+    rep.add("T2-amplitude equation", t2_eqn)
 
 """
 from gristmill import get_flop_cost
@@ -73,6 +78,3 @@ printer = EinsumPrinter()
 with open('rccd_equations.txt', 'w') as outfile:
     outfile.write(printer.doprint(working_eqn))
 """
-
-
-
