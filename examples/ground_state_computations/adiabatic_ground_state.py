645c44b06187b135f214b62f9f67cafbcf04e5e7
haakoek <haakoek@student.matnat.uio.no>
2020-02-13 21:13:12 +0100

    evaluate amplitude residuals

--- examples/ground_state_computations/adiabatic_ground_state.py
+++ examples/ground_state_computations/adiabatic_ground_state.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from coupled_cluster.integrators import GaussIntegrator
 from coupled_cluster import OATDCCD
 from coupled_cluster.cc_helper import OACCVector
+from coupled_cluster.ccd.rhs_t import compute_t_2_amplitudes
+from coupled_cluster.ccd.rhs_l import compute_l_2_amplitudes
 
 from quantum_systems import ODQD
 from quantum_systems.time_evolution_operators import AdiabaticSwitching
@@ -22,7 +24,7 @@ class FermiFunction:
 
 
 n = 4
-l = 10
+l = 6
 
 system = ODQD(n, l, 10, 201)
 system.setup_system(potential=ODQD.HOPotential(omega=1))
@@ -40,17 +42,45 @@ dt = 1e-1
 time_points = np.arange(0, 100 + dt, dt)
 
 energy = np.zeros(len(time_points), dtype=np.complex128)
+norm_res_t2 = np.zeros(len(time_points))
+norm_res_l2 = np.zeros(len(time_points))
+
 oa.u = np.zeros_like(oa.u)
 oa.f = oa.h
+
 energy[0] = oa.compute_energy()
+
+t, l, C, C_tilde = oa.amplitudes
+t2, l2 = t[1], l[0]
+norm_res_t2[0] = np.linalg.norm(
+    compute_t_2_amplitudes(oa.f, oa.u, t2, system.o, system.v, np=np)
+)
+norm_res_l2[0] = np.linalg.norm(
+    compute_l_2_amplitudes(oa.f, oa.u, t2, l2, system.o, system.v, np=np)
+)
+
 print(energy[0])
+print(norm_res_t2[0], norm_res_l2[0])
 
 for i, amp in tqdm.tqdm(
     enumerate(oa.solve(time_points)), total=len(time_points) - 1
 ):
+    t, l, C, C_tilde = amp
     energy[i + 1] = oa.compute_energy()
-
+    t2, l2 = t[1], l[0]
+    norm_res_t2[i + 1] = np.linalg.norm(
+        compute_t_2_amplitudes(oa.f, oa.u, t2, system.o, system.v, np=np)
+    )
+    norm_res_l2[i + 1] = np.linalg.norm(
+        compute_l_2_amplitudes(oa.f, oa.u, t2, l2, system.o, system.v, np=np)
+    )
 
 print(f"Final energy: {energy[-1]}")
+plt.figure()
 plt.plot(time_points, energy.real)
+
+plt.figure()
+plt.semilogy(time_points, norm_res_t2, label="t2 residual")
+plt.semilogy(time_points, norm_res_t2, label="l2 residual")
+plt.legend()
 plt.show()
