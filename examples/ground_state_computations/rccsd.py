a38613f25af5faf787a0ace453378a65356ba712
haakoek <haakoek@student.matnat.uio.no>
2020-02-19 21:40:41 +0100

    remove explicit handling of restricted system. this assumes the new version of quantum system

--- examples/ground_state_computations/rccsd.py
+++ examples/ground_state_computations/rccsd.py
@@ -2,6 +2,7 @@ import numpy as np
 from quantum_systems import construct_pyscf_system_rhf
 
 molecule = "li 0.0 0.0 0.0;h 0.0 0.0 3.08"
+# molecule = "he 0.0 0.0 0.0"
 basis = "cc-pvdz"
 
 from coupled_cluster.mix import DIIS, AlphaMixer
@@ -15,14 +16,15 @@ system = construct_pyscf_system_rhf(
     add_spin=False,
     anti_symmetrize=False,
 )
+
 rccsd = RCCSD(system, mixer=DIIS, verbose=True)
 
-conv_tol = 1e-8
+conv_tol = 1e-10
 t_kwargs = dict(tol=conv_tol)
 l_kwargs = dict(tol=conv_tol)
 
 rccsd.compute_ground_state(t_kwargs=t_kwargs, l_kwargs=l_kwargs)
-print("Ground state energy: {0}".format(rccsd.compute_energy()))
+print("Ground state correlation energy: {0}".format(rccsd.compute_energy()))
 dm1 = rccsd.compute_one_body_density_matrix()
 for i in range(3):
     dip_xi = np.trace(np.dot(dm1, system.dipole_moment[i]))
