7fa39364f22962e18082b92c23eb1f01ad5d0037
haakoek <haakoek@student.matnat.uio.no>
2020-02-27 22:00:40 +0100

    black rccsd test

--- tests/test_rccsd_ground_state_properties.py
+++ tests/test_rccsd_ground_state_properties.py
@@ -3,8 +3,9 @@ from quantum_systems import construct_pyscf_system_rhf
 from coupled_cluster.mix import DIIS, AlphaMixer
 from coupled_cluster.rccsd import RCCSD
 
-def compute_ground_state_properties(molecule,basis):
-    
+
+def compute_ground_state_properties(molecule, basis):
+
     system = construct_pyscf_system_rhf(
         molecule,
         basis=basis,
@@ -13,7 +14,7 @@ def compute_ground_state_properties(molecule,basis):
         add_spin=False,
         anti_symmetrize=False,
     )
-    
+
     e_ref = system.compute_reference_energy()
     e_nuc = system.nuclear_repulsion_energy
 
@@ -24,26 +25,28 @@ def compute_ground_state_properties(molecule,basis):
     l_kwargs = dict(tol=conv_tol)
 
     rccsd.compute_ground_state(t_kwargs=t_kwargs, l_kwargs=l_kwargs)
-    e_rccsd = rccsd.compute_energy()+e_ref+e_nuc
+    e_rccsd = rccsd.compute_energy() + e_ref + e_nuc
 
     """
     Potentially compute more propeties such as dipole moment
     """
     return e_rccsd
 
+
 def test_rccsd():
-    
+
     molecule = "li 0.0 0.0 0.0;h 0.0 0.0 3.08"
     basis = "cc-pvdz"
     e_rccsd_dalton = -8.0147418652916809
-    e_rccsd = compute_ground_state_properties(molecule,basis)
-    assert(abs(e_rccsd-e_rccsd_dalton) < 1e-8)
+    e_rccsd = compute_ground_state_properties(molecule, basis)
+    assert abs(e_rccsd - e_rccsd_dalton) < 1e-8
 
     molecule = "be 0.0 0.0 0.0"
     basis = "aug-cc-pvdz"
     e_rccsd_dalton = -14.617433363077
-    e_rccsd = compute_ground_state_properties(molecule,basis)
-    assert(abs(e_rccsd-e_rccsd_dalton) < 1e-8)
-    
+    e_rccsd = compute_ground_state_properties(molecule, basis)
+    assert abs(e_rccsd - e_rccsd_dalton) < 1e-8
+
+
 if __name__ == "__main__":
     test_rccsd()
