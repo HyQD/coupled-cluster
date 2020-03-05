a953e1d079731457531bded7eff53fdbcd0e09e8
haakoek <haakoek@student.matnat.uio.no>
2020-03-05 11:40:14 +0100

    use new convention for system setup

--- examples/ground_state_computations/adiabatic_ground_state.py
+++ examples/ground_state_computations/adiabatic_ground_state.py
@@ -8,7 +8,7 @@ from coupled_cluster.cc_helper import OACCVector
 from coupled_cluster.ccd.rhs_t import compute_t_2_amplitudes
 from coupled_cluster.ccd.rhs_l import compute_l_2_amplitudes
 
-from quantum_systems import ODQD
+from quantum_systems import ODQD, GeneralOrbitalSystem
 from quantum_systems.time_evolution_operators import AdiabaticSwitching
 
 
@@ -19,19 +19,18 @@ class FermiFunction:
 
     def __call__(self, current_time):
         denom = 1 + np.exp((current_time - self.half_time) / self.tau)
-
         return 1 - 1 / denom
 
 
-n = 4
+n = 2
 l = 6
 
-system = ODQD(n, l, 10, 201)
-system.setup_system(potential=ODQD.HOPotential(omega=1))
+system = GeneralOrbitalSystem(n,ODQD(l, 7, 401,potential=ODQD.HOPotential(omega=1)))
 system.set_time_evolution_operator(AdiabaticSwitching(FermiFunction()))
 print(f"Reference energy: {system.compute_reference_energy()}")
 
-integrator = GaussIntegrator(s=3, eps=1e-6, np=np)
+integrator = GaussIntegrator(s=3, eps=1e-10, np=np)
+
 oa = OATDCCD(system, verbose=True, integrator=integrator)
 oa.compute_ground_state(change_system_basis=False)
 oa.set_initial_conditions(
@@ -39,7 +38,7 @@ oa.set_initial_conditions(
 )
 
 dt = 1e-1
-time_points = np.arange(0, 100 + dt, dt)
+time_points = np.arange(0, 70 + dt, dt)
 
 energy = np.zeros(len(time_points), dtype=np.complex128)
 norm_res_t2 = np.zeros(len(time_points))
