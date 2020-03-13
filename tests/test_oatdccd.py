Fix off-by-one bug in OATDCCD test

tests/test_oatdccd.py
@@ -148,14 +148,10 @@ def test_oatdccd_helium():
     oaccd.compute_ground_state()
     assert abs(oaccd.compute_energy() - -2.887_594_831_090_936) < 1e-6
 
-    amps0 = oaccd.get_amplitudes(get_t_0=True)
-    y0 = amps0.asarray()
-    print([v.shape for v in amps0.unpack()])
-
     oatdccd = OATDCCD(system)
 
     r = complex_ode(oatdccd).set_integrator("GaussIntegrator", s=3, eps=1e-6)
-    r.set_initial_value(y0)
+    r.set_initial_value(oaccd.get_amplitudes(get_t_0=True).asarray())
 
     polarization = np.zeros(3)
     polarization[2] = 1
@@ -176,31 +172,31 @@ def test_oatdccd_helium():
     td_energies = np.zeros(len(time_points), dtype=np.complex128)
     dip_z = np.zeros(len(time_points))
 
-    rho_qp = oatdccd.compute_one_body_density_matrix(r.t, r.y)
-    rho_qp_hermitian = 0.5 * (rho_qp.conj().T + rho_qp)
+    i = 0
 
-    td_energies[0] = oatdccd.compute_energy(r.t, r.y)
+    while r.successful() and r.t < T:
+        assert abs(time_points[i] - r.t) < dt * 1e-1
 
-    t, l, C, C_tilde = oatdccd.amplitudes_from_array(r.y)
-
-    z = C_tilde @ system.dipole_moment[2] @ C
-
-    dip_z[0] = np.einsum("qp,pq->", rho_qp_hermitian, z).real
-
-    for i, t in enumerate(time_points[:-1]):
-        r.integrate(r.t + dt)
-
-        if not r.successful():
-            break
-        td_energies[i + 1] = oatdccd.compute_energy(r.t, r.y)
-
+        td_energies[i] = oatdccd.compute_energy(r.t, r.y)
         rho_qp = oatdccd.compute_one_body_density_matrix(r.t, r.y)
         rho_qp_hermitian = 0.5 * (rho_qp.conj().T + rho_qp)
 
         t, l, C, C_tilde = oatdccd.amplitudes_from_array(r.y)
         z = C_tilde @ system.dipole_moment[2] @ C
 
-        dip_z[i + 1] = np.einsum("qp,pq->", rho_qp_hermitian, z).real
+        dip_z[i] = np.trace(rho_qp_hermitian @ z).real
+
+        i += 1
+        r.integrate(time_points[i])
+
+    td_energies[i] = oatdccd.compute_energy(r.t, r.y)
+    rho_qp = oatdccd.compute_one_body_density_matrix(r.t, r.y)
+    rho_qp_hermitian = 0.5 * (rho_qp.conj().T + rho_qp)
+
+    t, l, C, C_tilde = oatdccd.amplitudes_from_array(r.y)
+    z = C_tilde @ system.dipole_moment[2] @ C
+
+    dip_z[i] = np.trace(rho_qp_hermitian @ z).real
 
     np.testing.assert_allclose(
         td_energies.real,