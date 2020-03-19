2f4ca37740393a340d64199ad4558e3fd8a0a526
haakoek <haakoek@student.matnat.uio.no>
2020-03-19 15:35:30 +0100

    import restricetd density matrix

--- coupled_cluster/rccd/rccd.py
+++ coupled_cluster/rccd/rccd.py
@@ -1,7 +1,7 @@
 from coupled_cluster.cc import CoupledCluster
 from coupled_cluster.rccd.rhs_t import compute_t_2_amplitudes
 from coupled_cluster.rccd.rhs_l import compute_l_2_amplitudes
-from coupled_cluster.ccd.density_matrices import (
+from coupled_cluster.rccd.density_matrices import (
     compute_one_body_density_matrix,
     compute_two_body_density_matrix,
 )
