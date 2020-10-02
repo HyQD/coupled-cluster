a1cecdc3c4c75da21a838a3e95b2a8b245dcc120
haakoek <haakoek@student.matnat.uio.no>
2020-10-02 14:27:53 +0200

    black

--- coupled_cluster/omp2/tdomp2.py
+++ coupled_cluster/omp2/tdomp2.py
@@ -1,7 +1,5 @@
 from coupled_cluster.omp2.rhs_t import compute_t_2_amplitudes
-from coupled_cluster.omp2.energies import (
-    compute_ccd_ground_state_energy,
-)
+from coupled_cluster.omp2.energies import compute_ccd_ground_state_energy
 from coupled_cluster.omp2.density_matrices import (
     compute_one_body_density_matrix,
     compute_two_body_density_matrix,
