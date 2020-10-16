6b14c58e86fe51d9112790f73836a2d81d5e0353
haakoek <haakoek@student.matnat.uio.no>
2020-10-16 14:34:10 +0200

    remove use of utils

--- coupled_cluster/omp2/rhs_t.py
+++ coupled_cluster/omp2/rhs_t.py
@@ -1,6 +1,3 @@
-from coupled_cluster.utils import ndot
-
-
 def compute_t_2_amplitudes(f, u, t, o, v, np, out=None):
 
     nocc = t.shape[2]
@@ -9,11 +6,11 @@ def compute_t_2_amplitudes(f, u, t, o, v, np, out=None):
     r_T2 = np.zeros((nvirt, nvirt, nocc, nocc), dtype=t.dtype)
     r_T2 += u[v, v, o, o]
 
-    Pij = ndot("abik,kj->abij", t, f[o, o])
+    Pij = np.einsum("abik,kj->abij", t, f[o, o])
     r_T2 -= Pij
     r_T2 += Pij.swapaxes(2, 3)
 
-    Pab = ndot("ac,cbij->abij", f[v, v], t)
+    Pab = np.einsum("ac,cbij->abij", f[v, v], t)
     r_T2 += Pab
     r_T2 -= Pab.swapaxes(0, 1)
 
