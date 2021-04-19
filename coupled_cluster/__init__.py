b6fc8aa71a383127034a105214b2761d824f078e
haakoek <haakoek@student.matnat.uio.no>
2021-04-19 16:59:27 +0200

    update init files to include roatdccd class

--- coupled_cluster/__init__.py
+++ coupled_cluster/__init__.py
@@ -3,6 +3,6 @@ from .ccd import CCD, CoupledClusterDoubles, OACCD, TDCCD, OATDCCD
 from .omp2 import OMP2, TDOMP2
 from .ccsd import CCSD, CoupledClusterSinglesDoubles, TDCCSD
 from .rccsd import RCCSD, TDRCCSD
-from .rccd import RCCD, ROACCD
+from .rccd import RCCD, ROACCD, ROATDCCD
 from .rcc2 import RCC2, TDRCC2
 from .cc2 import CC2, TDCC2
