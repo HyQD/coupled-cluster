d19b095aecd75b2f6bef4233a0c6fd7a239ad520
haakoek <haakoek@student.matnat.uio.no>
2021-04-21 12:41:05 +0200

    test for energy conservation after pulse

--- tests/test_tdrccsd.py
+++ tests/test_tdrccsd.py
@@ -28,6 +28,85 @@ class sine_square_laser:
         return pulse
 
 
+def test_energy_conservation_after_pulse():
+
+    name = "beryllium"
+    atom = "be 0.0 0.0 0.0"
+    basis = "cc-pvdz"
+    charge = 0
+
+    system = construct_pyscf_system_rhf(
+        atom,
+        basis=basis,
+        np=np,
+        verbose=False,
+        add_spin=False,
+        anti_symmetrize=False,
+    )
+
+    F_str = 0.01
+    omega = 0.2
+
+    tprime = 1
+    phase = 0
+    polarization = np.zeros(3)
+    polarization_direction = 2
+    polarization[polarization_direction] = 1
+
+    time_after_pulse = 0
+    tfinal = 5
+
+    system.set_time_evolution_operator(
+        DipoleFieldInteraction(
+            sine_square_laser(
+                F_str=F_str, omega=omega, tprime=tprime, phase=phase
+            ),
+            polarization_vector=polarization,
+        )
+    )
+
+    s = 3
+    eps = 1e-5
+    dt = 1e-1
+
+    cc_kwargs = dict(verbose=False)
+    rccsd = RCCSD(system, **cc_kwargs)
+
+    ground_state_tolerance = 1e-8
+    rccsd.compute_ground_state(
+        t_kwargs=dict(tol=ground_state_tolerance),
+        l_kwargs=dict(tol=ground_state_tolerance),
+    )
+
+    amps0 = rccsd.get_amplitudes(get_t_0=True)
+    y0 = amps0.asarray()
+
+    tdrccsd = TDRCCSD(system)
+
+    r = complex_ode(tdrccsd).set_integrator("GaussIntegrator", s=3, eps=1e-6)
+    r.set_initial_value(y0)
+
+    num_steps = int(tfinal / dt) + 1
+    time_points = np.linspace(0, tfinal, num_steps)
+
+    # Initialize arrays to hold different "observables".
+    energy = np.zeros(num_steps, dtype=np.complex128)
+
+    # Set initial values
+    t, l = amps0
+    energy[0] = tdrccsd.compute_energy(r.t, r.y)
+
+    for i, _t in enumerate(time_points[:-1]):
+        r.integrate(r.t + dt)
+
+        if not r.successful():
+            break
+        t, l = amps0.from_array(r.y)
+        energy[i + 1] = tdrccsd.compute_energy(r.t, r.y)
+
+    assert np.linalg.norm(energy[11:].real - energy[11].real) < 1e-9
+
+
 def test_tdrccsd_vs_tdccsd():
     name = "beryllium"
     atom = "be 0.0 0.0 0.0"
@@ -171,4 +250,4 @@ def test_tdrccsd_vs_tdccsd():
 
 
 if __name__ == "__main__":
-    test_tdrccsd_vs_tdccsd()
+    test_energy_conservation_after_pulse()
