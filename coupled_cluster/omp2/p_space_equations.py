00ca65f05c4ea626a7a4731e518e90efaf188cf8
haakoek <haakoek@student.matnat.uio.no>
2021-05-10 17:54:08 +0200

    Function for eta right hand side

--- coupled_cluster/omp2/p_space_equations.py
+++ coupled_cluster/omp2/p_space_equations.py
@@ -1,21 +1,7 @@
 def compute_eta(h, u, rho_qp, rho_qspr, o, v, np):
 
     # Eq. (23) in: https://aip.scitation.org/doi/10.1063/1.5020633
-    R_ai = np.einsum("aj,ji->ai", h[v, o], rho_qp[o, o])
-    R_ai += 0.5 * np.einsum(
-        "arqs,qsir->ai",
-        u[v, :, :, :],
-        rho_qspr[:, :, o, :],
-        optimize=True,
-    )
-
-    R_ai -= np.einsum("ab, bi->ai", rho_qp[v, v], h[v, o])
-    R_ai -= 0.5 * np.einsum(
-        "qsir,arqs->ai",
-        u[:, :, o, :],
-        rho_qspr[v, :, :, :],
-        optimize=True,
-    )
+    R_ai = compute_R_tilde_ai(h, u, rho_qp, rho_qspr, o, v, np)
 
     # Solve P-space equations for X^b_j
     delta_ij = np.eye(o.stop)
@@ -31,3 +17,18 @@ def compute_eta(h, u, rho_qp, rho_qspr, o, v, np):
     X[o, v] = -X_bj.T.conj()
 
     return X
+
+
+def compute_R_tilde_ai(h, u, rho_qp, rho_qspr, o, v, np):
+
+    R_ai = np.einsum("aj,ji->ai", h[v, o], rho_qp[o, o])
+    R_ai += 0.5 * np.einsum(
+        "arqs,qsir->ai", u[v, :, :, :], rho_qspr[:, :, o, :], optimize=True
+    )
+
+    R_ai -= np.einsum("ab, bi->ai", rho_qp[v, v], h[v, o])
+    R_ai -= 0.5 * np.einsum(
+        "qsir,arqs->ai", u[:, :, o, :], rho_qspr[v, :, :, :], optimize=True
+    )
+
+    return R_ai
