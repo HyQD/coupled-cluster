8fe83d4b1542c046c6f7049651036eeed534669d
haakoek <haakoek@student.matnat.uio.no>
2021-05-18 15:16:06 +0200

    Make eta right hand sides consistent with those in the general omp2 method.

--- coupled_cluster/romp2/p_space_equations.py
+++ coupled_cluster/romp2/p_space_equations.py
@@ -1,22 +1,48 @@
 def compute_eta(h, u, rho_qp, rho_qspr, o, v, np):
+    eta = np.zeros(h.shape, dtype=np.complex128)
+    A_ibaj = compute_A_ibaj(rho_qp, o, v, np=np)
+    R_ia = compute_R_ia(h, u, rho_qp, rho_qspr, o, v, np=np)
 
-    # Eq. (23) in: https://aip.scitation.org/doi/10.1063/1.5020633
-    R_ai = compute_R_tilde_ai(h, u, rho_qp, rho_qspr, o, v, np)
+    A_iajb = A_ibaj.transpose(0, 2, 3, 1)
+    eta_jb = -1j * np.linalg.tensorsolve(A_iajb, R_ia)
 
-    # Solve P-space equations for X^b_j
+    eta[o, v] += eta_jb
+    eta[v, o] -= eta_jb.conj().T
+
+    return eta
+
+
+def compute_A_ibaj(rho_qp, o, v, np):
     delta_ij = np.eye(o.stop)
     delta_ba = np.eye(v.stop - o.stop)
 
-    A_aibj = np.einsum("ab, ji -> aibj", delta_ba, rho_qp[o, o])
-    A_aibj -= np.einsum("ji, ab -> aibj", delta_ij, rho_qp[v, v])
+    A_ibaj = np.einsum("ba, ij -> ibaj", delta_ba, rho_qp[o, o])
+    A_ibaj -= np.einsum("ij, ba -> ibaj", delta_ij, rho_qp[v, v])
 
-    X_bj = -1j * np.linalg.tensorsolve(A_aibj, R_ai)
-    X = np.zeros(h.shape, dtype=np.complex128)
+    return A_ibaj
 
-    X[v, o] = X_bj
-    X[o, v] = -X_bj.T.conj()
 
-    return X
+def compute_R_ia(h, u, rho_qp, rho_qspr, o, v, np):
+    R_ia = np.dot(rho_qp[o, o], h[o, v])
+    R_ia -= np.dot(h[o, v], rho_qp[v, v])
+    R_ia += np.tensordot(
+        # rho^{is}_{pr}
+        rho_qspr[o, :, :, :],
+        # u^{pr}_{as}
+        u[:, :, v, :],
+        # axes=((s, p, r), (s, p, r))
+        axes=((1, 2, 3), (3, 0, 1)),
+    )
+    R_ia -= np.tensordot(
+        # u^{ir}_{qs}
+        u[o, :, :, :],
+        # rho^{qs}_{ar}
+        rho_qspr[:, :, v, :],
+        # axes=((r, q, s), (r, q, s))
+        axes=((1, 2, 3), (3, 0, 1)),
+    )
+
+    return R_ia
 
 
 def compute_R_tilde_ai(h, u, rho_qp, rho_qspr, o, v, np):
