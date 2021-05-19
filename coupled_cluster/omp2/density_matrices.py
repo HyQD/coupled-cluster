a4f75415b5f5363155dd32547a21fc48e3884524
haakoek <haakoek@student.matnat.uio.no>
2021-05-19 15:51:19 +0200

    use opt_einsum

--- coupled_cluster/omp2/density_matrices.py
+++ coupled_cluster/omp2/density_matrices.py
@@ -1,3 +1,6 @@
+from opt_einsum import contract
+
+
 def compute_one_body_density_matrix(t, l, o, v, np, out=None):
 
     nocc = t.shape[2]
@@ -9,10 +12,10 @@ def compute_one_body_density_matrix(t, l, o, v, np, out=None):
     opdm[o, o] += np.identity(o.stop)
 
     # Build one particle density matrix
-    opdm[v, v] += (1 / 2) * np.einsum(
+    opdm[v, v] += (1 / 2) * contract(
         "ijac,bcij -> ba", t.T.conj(), t, optimize=True
     )
-    opdm[o, o] += -(1 / 2) * np.einsum(
+    opdm[o, o] += -(1 / 2) * contract(
         "jkab,abik -> ji", t.T.conj(), t, optimize=True
     )
 
@@ -30,19 +33,19 @@ def compute_two_body_density_matrix(t, l, o, v, np, out=None):
     ################################################################
     delta = np.eye(o.stop)
 
-    term = np.einsum("ki, lj -> klij", delta, delta)
+    term = contract("ki, lj -> klij", delta, delta)
     term -= term.swapaxes(2, 3)
     tpdm[o, o, o, o] += term
 
     term_lj = -0.5 * np.tensordot(t.T.conj(), t, axes=((1, 2, 3), (3, 0, 1)))
-    term = np.einsum("ki, lj -> klij", delta, term_lj)
+    term = contract("ki, lj -> klij", delta, term_lj)
     term -= term.swapaxes(0, 1)
     term -= term.swapaxes(2, 3)
     tpdm[o, o, o, o] += term
 
     # Complexity: O(n^2 m^3)
     W_ba = 0.5 * np.tensordot(t, t.T.conj(), axes=((1, 2, 3), (3, 0, 1)))
-    term_jbia = np.einsum("ji, ba -> jbia", delta, W_ba)
+    term_jbia = contract("ji, ba -> jbia", delta, W_ba)
 
     # rho^{jb}_{ia}
     tpdm[o, v, o, v] += term_jbia
