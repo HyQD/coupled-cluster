76a6523c27bc878cfc7d246165bb6cc57d03a26e
haakoek <h.e.kristiansen@kjemi.uio.no>
2022-05-12 11:02:28 +0200

    new black

--- coupled_cluster/rccd/p_space_equations.py
+++ coupled_cluster/rccd/p_space_equations.py
@@ -94,9 +94,9 @@ def compute_R_tilde_ai(h, u, rho_qp, rho_qspr, o, v, np):
 
 
 def compute_R_ia_compact(h, u, rho_qp, rho_qspr, o, v, np):
-    
+
     """
-    The use of ":"-slices leads to unpredictable performance. 
+    The use of ":"-slices leads to unpredictable performance.
     However, the function is kept for testing purposes.
     """
 
@@ -112,10 +112,10 @@ def compute_R_ia_compact(h, u, rho_qp, rho_qspr, o, v, np):
 def compute_R_tilde_ai_compact(h, u, rho_qp, rho_qspr, o, v, np):
 
     """
-    The use of ":"-slices leads to unpredictable performance. 
+    The use of ":"-slices leads to unpredictable performance.
     However, the function is kept for testing purposes.
     """
-    
+
     R_tilde_ai = np.dot(rho_qp[v, v], h[v, o])
     R_tilde_ai -= np.dot(h[v, o], rho_qp[o, o])
     R_tilde_ai += contract(
